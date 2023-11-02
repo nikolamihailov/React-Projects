686663a02189ca1b2d4b77a1fbac4828f31ebf06
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-02 23:03:45 +0200

    topNav structure done

---
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/TopNav.jsx
@@ -0,0 +1,14 @@
+const TopNav = () => {
+  return (
+    <nav>
+      <div>
+        {/* logo */}
+        <div></div>
+        {/*  // icons navigation */}
+        <div></div>
+      </div>
+    </nav>
+  );
+};
+
+export default TopNav;

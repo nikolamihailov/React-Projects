0fe86c2a59c1bdbb5e7d98cd32bba93e740dd0c1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-03 12:26:27 +0200

    app theme changes for theme toggle

--- kolplace - Final React Project SoftUni/client/src/App.jsx
+++ kolplace - Final React Project SoftUni/client/src/App.jsx
@@ -1,17 +1,27 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import TopNav from "./components/Navigation/TopNavigation/TopNav";
+import ThemeButton from "./components/ThemeTogller/ThemeButton";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
   // dynamically set page title
   useEffect(() => {
     document.title = "KolPlace - Your Online Shop";
   }, []);
 
+  // change app theme class on theme on btn click
+  const toggleTheme = () => {
+    if (theme === "light") setTheme("dark");
+    else setTheme("light");
+  };
+
   return (
-    <>
+    <div className={`app-${theme}`}>
       <TopNav />
-    </>
+      <ThemeButton toggleTheme={toggleTheme} />
+    </div>
   );
 }
 

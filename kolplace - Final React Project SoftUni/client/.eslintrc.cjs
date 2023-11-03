f9f593672d8cd7a1efb5b0f3fb8e7e04a5265241
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-03 12:24:23 +0200

    disabled prop/types validation because of no ts

--- kolplace - Final React Project SoftUni/client/.eslintrc.cjs
+++ kolplace - Final React Project SoftUni/client/.eslintrc.cjs
@@ -12,9 +12,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/prop-types": 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};

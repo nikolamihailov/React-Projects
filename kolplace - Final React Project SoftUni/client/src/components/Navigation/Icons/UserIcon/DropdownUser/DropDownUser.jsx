e21fd4e2b2a68d8d14781497aa05802416506532
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-05 00:25:30 +0200

    dropdowns css fixed

--- kolplace - Final React Project SoftUni/client/src/components/Navigation/Icons/UserIcon/DropdownUser/DropDownUser.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/Icons/UserIcon/DropdownUser/DropDownUser.jsx
@@ -2,8 +2,15 @@ import styles from "./Dropdown.module.css";
 const DropDownUser = () => {
   return (
     <ul className={styles["user-dropdown"]}>
-      <li>Login</li>
-      <li>Register</li>
+      <li>
+        <i className="fa-solid fa-key"></i>
+        <span>Login</span>
+      </li>
+
+      <li>
+        <i className="fa-solid fa-user-plus"></i>
+        <span>Register</span>
+      </li>
     </ul>
   );
 };

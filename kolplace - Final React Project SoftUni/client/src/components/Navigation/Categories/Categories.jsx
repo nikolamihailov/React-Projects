2688888c1360399c41606a7c7b47d97ccf017859
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-05 00:25:16 +0200

    hover updated

--- kolplace - Final React Project SoftUni/client/src/components/Navigation/Categories/Categories.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/Categories/Categories.jsx
@@ -1,28 +1,28 @@
-// import { useState } from "react";
+import { useState } from "react";
 import styles from "./Categories.module.css";
 import CategoriesDropdown from "./CategoriesDropdown/CategoriesDropdown";
 
 const Categories = () => {
-  /*   const [isDropdownOpen, setIsDropDownOpen] =
-    useState(true); */
+  const [isDropdownOpen, setIsDropDownOpen] =
+    useState(true);
 
-  /*   const handleMouseEnter = () => {
+  const handleMouseEnter = () => {
     setIsDropDownOpen(true);
   };
 
   const handleMouseLeave = () => {
     setIsDropDownOpen(false);
-  }; */
+  };
 
   return (
     <div
       className={styles["categories-btn-container"]}
-      /*  onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave} */
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <i className="fa-solid fa-list"></i>
       <button>Categories</button>
-      {1 === 1 && <CategoriesDropdown />}
+      {isDropdownOpen && <CategoriesDropdown />}
     </div>
   );
 };

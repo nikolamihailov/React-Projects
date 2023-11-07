c21c4b6a5b66e85ada02f92c450ad97009e0ff80
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-07 13:34:30 +0200

    home page added

---
+++ kolplace - Final React Project SoftUni/client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import useTitle from "../../hooks/useTitle";
+
+const Home = () => {
+  useTitle("KolPlace - Your Online Shop");
+  return (
+    <section>
+      <h1>Home Page</h1>
+    </section>
+  );
+};
+
+export default Home;

1dfc91c5fc92d63f8a9c73107219325c13723ec7
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-10 23:04:53 +0200

    AuthProvider added

--- kolplace - Final React Project SoftUni/client/src/App.jsx
+++ kolplace - Final React Project SoftUni/client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { ThemeContext } from "./contexts/themeContext.js";
 import changeTheme from "./assets/js/theme.js";
 import Header from "./components/Header/Header";
 import Login from "./components/Auth/Login/Login.jsx";
@@ -8,6 +7,8 @@ import Home from "./components/Home/Home.jsx";
 import Register from "./components/Auth/Register/Register.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import "./App.css";
+import { ThemeContext } from "./contexts/ThemeContext.js";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 function App() {
   const [theme, setTheme] = useState("");
@@ -27,25 +28,27 @@ function App() {
   };
 
   return (
-    <ThemeContext.Provider value={values}>
-      <div className={`app ${theme}`}>
-        <Header />
-        <main className="main">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route
-              path="/login"
-              element={<Login />}
-            ></Route>
-            <Route
-              path="/register"
-              element={<Register />}
-            ></Route>
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </ThemeContext.Provider>
+    <AuthProvider>
+      <ThemeContext.Provider value={values}>
+        <div className={`app ${theme}`}>
+          <Header />
+          <main className="main">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route
+                path="/login"
+                element={<Login />}
+              ></Route>
+              <Route
+                path="/register"
+                element={<Register />}
+              ></Route>
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
+    </AuthProvider>
   );
 }
 

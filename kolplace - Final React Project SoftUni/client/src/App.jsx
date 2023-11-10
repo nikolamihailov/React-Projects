c1b5a3fe168bd0abbc1a402ccedcd9d1a55790f6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-10 22:25:11 +0200

    updated

--- kolplace - Final React Project SoftUni/client/src/App.jsx
+++ kolplace - Final React Project SoftUni/client/src/App.jsx
@@ -1,20 +1,16 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { ThemeContext } from "./contexts/themeContext.js";
 import changeTheme from "./assets/js/theme.js";
 import Header from "./components/Header/Header";
-import { ThemeContext } from "./contexts/themeContext.js";
-import "./App.css";
 import Login from "./components/Auth/Login/Login.jsx";
-import useTitle from "./hooks/useTitle.js";
 import Home from "./components/Home/Home.jsx";
 import Register from "./components/Auth/Register/Register.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import "./App.css";
 
 function App() {
   const [theme, setTheme] = useState("");
-
-  // dynamically set page title
-  useTitle("KolPlace - Your Shopping Place");
   useEffect(() => {
     setTheme(changeTheme());
   }, []);

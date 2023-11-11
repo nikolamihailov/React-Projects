7a5d29d96284f9a196027ff4d746593b399b7bca
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-11 22:30:14 +0200

    express config file made

---
+++ kolplace - Final React Project SoftUni/server/src/config/expressConfig.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+// required for .env variables as well as npm i dotenv
+require('dotenv').config();
+const PORT = process.env.PORT || 3030;
+
+const startServer = () => {
+    const app = express();
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(cors());
+    app.listen(PORT, () => console.log("Server is listening on port " + PORT));
+};
+
+module.exports = startServer;
+
+
+
--- kolplace - Final React Project SoftUni/server/src/index.js
+++ kolplace - Final React Project SoftUni/server/src/index.js
@@ -1,12 +1,4 @@
-const express = require("express");
-// required for .env variables as well as npm i dotenv
-require('dotenv').config();
-const PORT = process.env.PORT || 3030;
+const startServer = require("./config/expressConfig");
 
-const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Started");
-});
-
-app.listen(PORT, () => console.log("Server is listening on port " + PORT));
+startServer();

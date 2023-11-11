dc15359d8c363c8905e2cd74d39a187adc8cc6e4
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-11 22:44:10 +0200

    added routes to config

--- kolplace - Final React Project SoftUni/server/src/config/expressConfig.js
+++ kolplace - Final React Project SoftUni/server/src/config/expressConfig.js
@@ -5,11 +5,14 @@ const cors = require("cors");
 require('dotenv').config();
 const PORT = process.env.PORT || 3030;
 
+const routes = require("../routes");
+
 const startServer = () => {
     const app = express();
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
     app.use(cors());
+    app.use(routes);
     app.listen(PORT, () => console.log("Server is listening on port " + PORT));
 };
 

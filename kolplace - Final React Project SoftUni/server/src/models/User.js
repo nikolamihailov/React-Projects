f88d0b4a1c434a868f176bcba2289d2d4b90a705
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-11 23:04:39 +0200

    user model added

---
+++ kolplace - Final React Project SoftUni/server/src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required!"],
+        minLength: [3, "First name must be at least 3 characters!"],
+        maxLength: [15, "First name must not be more than 15 characters!"],
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required!"],
+        minLength: [3, "Last name must be at least 3 characters!"],
+        maxLength: [15, "Last name must not be more than 15 characters!"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required!"],
+        minLength: [7, "Email must be at least 7 characters!"],
+        maxLength: [25, "Email must not be more than 25 characters!"],
+        unique: true,
+        validate: {
+            validator: async function (email) {
+                const user = await User.findOne({ email });
+                return !user;
+            },
+            message: 'Email already exists!',
+        },
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!"],
+        minLength: [6, "Password must be at least 6 characters!"],
+        maxLength: [20, "Password must not be more than 20 characters!"],
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

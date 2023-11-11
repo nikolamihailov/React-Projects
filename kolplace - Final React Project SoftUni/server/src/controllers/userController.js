9a80e168cb8cd11b39367f5a2fa799bfc43a8259
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-11 23:15:49 +0200

    simple register added

--- kolplace - Final React Project SoftUni/server/src/controllers/userController.js
+++ kolplace - Final React Project SoftUni/server/src/controllers/userController.js
@@ -1,11 +1,13 @@
 const userController = require("express").Router();
+const userService = require("../services/userService");
 
 userController.post("/login", (req, res) => {
     res.json({ "login": "ok" });
 });
 
-userController.post("/register", (req, res) => {
-    res.json({ "register": "ok" });
+userController.post("/register", async (req, res) => {
+    const user = await userService.register({ ...req.body });
+    res.json(user);
 });
 
 module.exports = userController;

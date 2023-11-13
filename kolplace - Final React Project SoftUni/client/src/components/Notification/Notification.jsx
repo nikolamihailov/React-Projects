82a16d8aca5657873d4b141fa67a091a347f7d20
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-14 00:41:16 +0200

    fixed

--- kolplace - Final React Project SoftUni/client/src/components/Notification/Notification.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Notification/Notification.jsx
@@ -5,22 +5,16 @@ const Notification = ({ text, type }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
-    // Set a timeout to trigger the unmounting process after 5 seconds
     const timeoutId = setTimeout(() => {
       setIsVisible(false);
     }, 5000);
-
-    // Clean up the timeout when the component unmounts or when the timeout triggers
     return () => clearTimeout(timeoutId);
-  }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
+  }, []);
 
-  // Return null if the component should be hidden
   if (!isVisible) {
     return null;
   }
-  const classes = `notification-${
-    type === "error" ? "error" : "success"
-  }`;
+  const classes = `notification-${type === "error" ? "error" : "success"}`;
   const startType = type === "error" ? "X" : "✓";
   return (
     <div className={styles[classes]}>

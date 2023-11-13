f60c7ed66b40c405206126dba7165c4953419bf8
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-13 21:01:29 +0200

    updated login, register

--- kolplace - Final React Project SoftUni/server/src/controllers/userController.js
+++ kolplace - Final React Project SoftUni/server/src/controllers/userController.js
@@ -1,13 +1,27 @@
 const userController = require("express").Router();
 const userService = require("../services/userService");
+const { trimBody } = require("../middlewares/trimBody");
+const { extractErrors } = require("../utils/errParse");
 
-userController.post("/login", (req, res) => {
-    res.json({ "login": "ok" });
+userController.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userData = await userService.login(email, password);
+        res.status(200).json(userData);
+    } catch (error) {
+        const errors = extractErrors(error);
+        res.status(401).json({ errors });
+    }
 });
 
-userController.post("/register", async (req, res) => {
-    const user = await userService.register({ ...req.body });
-    res.json(user);
+userController.post("/register", trimBody, async (req, res) => {
+    try {
+        const userData = await userService.register({ ...req.body });
+        res.status(201).json(userData);
+    } catch (error) {
+        const errors = extractErrors(error);
+        res.status(400).json({ errors });
+    }
 });
 
 module.exports = userController;

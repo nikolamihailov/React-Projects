e2730c55be4c7fb014d427f8fb10113326143aaf
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-13 21:01:49 +0200

    updated

--- kolplace - Final React Project SoftUni/server/src/services/userService.js
+++ kolplace - Final React Project SoftUni/server/src/services/userService.js
@@ -1,3 +1,41 @@
 const User = require("../models/User");
+const jwt = require("../utils/jwt");
+const bcrypt = require("bcrypt");
+const SECRET = process.env.JWT_SECRET;
 
-exports.register = (data) => User.create(data);
+exports.register = async (data) => {
+
+    const user = await User.create(data);
+
+    if (user) {
+        const token = await generateToken(user._id);
+        return {
+            _id: user._id,
+            email: user.email,
+            token,
+            role: user.role
+        };
+    }
+};
+
+exports.login = async (email, password) => {
+
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Invalid email or password!");
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) throw new Error("Invalid email or password!");
+
+    const token = await generateToken(user._id);
+    return {
+        _id: user._id,
+        email: user.email,
+        token,
+        role: user.role
+    };
+
+};
+
+async function generateToken(id) {
+    return await jwt.sign({ id }, SECRET, { expiresIn: "1d" });
+}

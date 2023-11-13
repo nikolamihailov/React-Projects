b371920dd705e27c5ce015a1e84f5a3501c5240c
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-13 21:04:21 +0200

    saving user in local storage added

--- kolplace - Final React Project SoftUni/client/src/contexts/AuthContext.jsx
+++ kolplace - Final React Project SoftUni/client/src/contexts/AuthContext.jsx
@@ -1,21 +1,54 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  register,
+  login,
+} from "../data/services/userService";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useLocalStorage(
+    "kolplace-auth",
+    {}
+  );
+  const [errors, setErrors] = useState([]);
+
   const navigateTo = useNavigate();
-  const onRegisterSubmit = (values) => {
-    console.log(values);
-    navigateTo("/");
+
+  const onRegisterSubmit = async (values) => {
+    setErrors([]);
+    const userData = await register(values);
+
+    if (userData.errors) {
+      setErrors(Object.values(userData.errors));
+    } else {
+      setAuth(userData);
+      setErrors([]);
+      navigateTo("/");
+    }
   };
-  const onLoginSubmit = (values) => {
-    console.log(values);
-    //navigateTo("/");
+
+  const onLoginSubmit = async (values) => {
+    setErrors([]);
+    const userData = await login(values);
+
+    if (userData.errors) {
+      setErrors(Object.values(userData.errors));
+    } else {
+      setAuth(userData);
+      setErrors([]);
+      navigateTo("/");
+    }
   };
   const ctxValues = {
     onRegisterSubmit,
     onLoginSubmit,
+    errors,
+    isAuthenticated: !!auth.token,
+    isAdmin: auth.role === "admin",
+    auth,
   };
   return (
     <AuthContext.Provider value={ctxValues}>

cda6b07759b37bf4e7fd989680251bf15b22049b
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-13 21:05:10 +0200

    fixed dropdown for logged users

--- kolplace - Final React Project SoftUni/client/src/components/Navigation/Icons/UserIcon/DropdownUser/DropDownUser.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/Icons/UserIcon/DropdownUser/DropDownUser.jsx
@@ -1,21 +1,60 @@
+import { useContext } from "react";
 import styles from "./Dropdown.module.css";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../../../contexts/AuthContext";
 const DropDownUser = () => {
+  const { isAdmin, isAuthenticated } =
+    useContext(AuthContext);
   return (
     <ul className={styles["user-dropdown"]}>
-      <Link to={"/login"}>
-        <li>
-          <i className="fa-solid fa-key"></i>
-          <span>Login</span>
-        </li>
-      </Link>
+      {isAuthenticated ? (
+        <>
+          <Link to={"/my-profile"}>
+            <li>
+              <i className="fa-solid fa-key"></i>
+              <span>My Profile</span>
+            </li>
+          </Link>
 
-      <Link to={"/register"}>
-        <li>
-          <i className="fa-solid fa-user-plus"></i>
-          <span>Register</span>
-        </li>
-      </Link>
+          <Link to={"/favourite-products"}>
+            <li>
+              <i className="fa-solid fa-user-plus"></i>
+              <span>Favourite Products</span>
+            </li>
+          </Link>
+          <Link to={"/logout"}>
+            <li>
+              <i className="fa-solid fa-user-plus"></i>
+              <span>Logout</span>
+            </li>
+          </Link>
+
+          {isAdmin && (
+            <Link to={"/admin"}>
+              <li>
+                <i className="fa-solid fa-key"></i>
+                <span>Admin Panel</span>
+              </li>
+            </Link>
+          )}
+        </>
+      ) : (
+        <>
+          <Link to={"/login"}>
+            <li>
+              <i className="fa-solid fa-key"></i>
+              <span>Login</span>
+            </li>
+          </Link>
+
+          <Link to={"/register"}>
+            <li>
+              <i className="fa-solid fa-user-plus"></i>
+              <span>Register</span>
+            </li>
+          </Link>
+        </>
+      )}
     </ul>
   );
 };

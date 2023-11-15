5c0a76dd94836ba8e8d86d30ec29f598c9c029bb
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-15 23:40:57 +0200

    home admin page updated

--- kolplace - Final React Project SoftUni/client/src/components/Admin/Admin.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Admin.jsx
@@ -1,9 +1,37 @@
+import { Outlet, useLocation } from "react-router-dom";
 import NavigationAside from "../Navigation/NavigationAdmin/NavigationAside/NavigationAside";
+import TopNavAdmin from "../Navigation/NavigationAdmin/NavigationTop/TopNavAdmin";
+import styles from "./Admin.module.css";
+import { useEffect, useState } from "react";
+import useTitle from "../../hooks/useTitle";
 
 const AdminPanel = () => {
+  useTitle("Admin Panel");
+  const [pageURL, setPageURL] = useState("/");
+  const loc = useLocation();
+
+  useEffect(() => {
+    setPageURL(loc.pathname);
+  }, [loc]);
+
   return (
-    <section>
-      <NavigationAside />
+    <section className={styles["admin-panel-section"]}>
+      <TopNavAdmin />
+      <div className={styles["content-container"]}>
+        <NavigationAside />
+        {pageURL === "/admin-panel" ? (
+          <div className={styles["admin-panel-info"]}>
+            <h1>Welcome to Admin Panel!</h1>
+            <p>
+              Here you can manage categories, products, users and stores! From
+              the navigation aside you can choose what to manage. To go back to
+              the webiste click the logo.
+            </p>
+          </div>
+        ) : (
+          <Outlet />
+        )}
+      </div>
     </section>
   );
 };

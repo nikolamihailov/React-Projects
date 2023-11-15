ee44b8769692a43ad86912e60ef622ef39d7cbef
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-15 23:41:58 +0200

    added categories page for admin

---
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Categories/Categories.jsx
@@ -0,0 +1,11 @@
+import styles from "./Categories.module.css";
+
+const Categories = () => {
+  return (
+    <div className={styles["admin-categories"]}>
+      <h1>All Categories</h1>
+    </div>
+  );
+};
+
+export default Categories;

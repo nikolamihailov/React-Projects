8845fb1f4319819433576e3ce4d6f5d0c572c818
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-15 16:47:51 +0200

    our stores page added

---
+++ kolplace - Final React Project SoftUni/client/src/components/OurStores/OurStores.jsx
@@ -0,0 +1,12 @@
+import useTitle from "../../hooks/useTitle";
+
+const OurStores = () => {
+  useTitle("Our Stores | KolPlace");
+
+  return (
+    <section>
+      <h1>Our Stores</h1>
+    </section>
+  );
+};
+export default OurStores;

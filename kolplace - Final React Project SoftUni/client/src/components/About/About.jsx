b1c319c99943a713ea7e18ac91a3471b44dc74fb
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-15 16:47:38 +0200

    about page added

---
+++ kolplace - Final React Project SoftUni/client/src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import useTitle from "../../hooks/useTitle";
+
+const About = () => {
+  useTitle("About Us | KolPlace");
+
+  return (
+    <section>
+      <h1>About us</h1>
+    </section>
+  );
+};
+export default About;

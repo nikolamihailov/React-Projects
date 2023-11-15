88e8e96777618d08c10170b5c10a1877258d345f
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-15 23:42:45 +0200

    one product added for testing

--- kolplace - Final React Project SoftUni/client/src/components/Home/Home.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Home/Home.jsx
@@ -6,6 +6,24 @@ const Home = () => {
   return (
     <section className={styles["home"]}>
       <h1>Home Page</h1>
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: "15px",
+        }}
+      >
+        <img
+          src="https://smartphone.bg/system/images/421908/original/mtv73rxa.png"
+          alt=""
+          style={{
+            height: "300px",
+            width: "400px",
+            filter: "drop-shadow(0 0 3px var(--primary-color))",
+          }}
+        />
+        <h2>Iphone 15 Pro Max</h2>
+        <p>$999</p>
+      </div>
     </section>
   );
 };

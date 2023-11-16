a6984b632033755614b036c67f08eee35588d5ef
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-16 23:29:38 +0200

    added mdw to app

--- kolplace - Final React Project SoftUni/server/src/config/expressConfig.js
+++ kolplace - Final React Project SoftUni/server/src/config/expressConfig.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { auth } = require("../middlewares/authMiddleware");
 
 // required for .env variables as well as npm i dotenv
 require('dotenv').config();
@@ -13,6 +14,7 @@ const startServer = () => {
     app.use(express.json());
     app.use(cors());
     app.use(routes);
+    app.use(auth);
     app.listen(PORT, () => console.log("Server is listening on port " + PORT));
 };
 

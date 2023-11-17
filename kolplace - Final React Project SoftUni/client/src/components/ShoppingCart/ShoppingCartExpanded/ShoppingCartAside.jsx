d4232db04c9bc7d079667d51dce9a285b93cc051
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-17 23:42:05 +0200

    added backdrop for shopping cart

--- kolplace - Final React Project SoftUni/client/src/components/ShoppingCart/ShoppingCartExpanded/ShoppingCartAside.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/ShoppingCart/ShoppingCartExpanded/ShoppingCartAside.jsx
@@ -2,25 +2,28 @@ import styles from "./Aside.module.css";
 
 const ShoppingCartExpanded = ({ onClick }) => {
   return (
-    <aside className={styles["cart-aside"]}>
-      <div onClick={onClick}>
-        <i className="fa-solid fa-x"></i>
-      </div>
-      <div className={styles["cart-item-container"]}>
-        <div className={styles["cart-item"]}>
-          <img
-            src="https://smartphone.bg/system/images/421908/original/mtv73rxa.png"
-            alt=""
-          />
-          <h4>Ihone 15 Pro Max</h4>
-          <p>$999</p>
+    <>
+      <div className="backdrop" onClick={onClick}></div>
+      <aside className={styles["cart-aside"]}>
+        <div onClick={onClick}>
+          <i className="fa-solid fa-x"></i>
         </div>
-      </div>
-      <button>
-        <i className="fa-solid fa-cart-shopping"></i>
-        View Cart
-      </button>
-    </aside>
+        <div className={styles["cart-item-container"]}>
+          <div className={styles["cart-item"]}>
+            <img
+              src="https://smartphone.bg/system/images/421908/original/mtv73rxa.png"
+              alt=""
+            />
+            <h4>Ihone 15 Pro Max</h4>
+            <p>$999</p>
+          </div>
+        </div>
+        <button>
+          <i className="fa-solid fa-cart-shopping"></i>
+          View Cart
+        </button>
+      </aside>
+    </>
   );
 };
 

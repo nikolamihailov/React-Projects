ae708d35a513d25ca06eeb703bccc0b3da9e8668
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-17 23:38:21 +0200

    controller added to routes

--- kolplace - Final React Project SoftUni/server/src/routes.js
+++ kolplace - Final React Project SoftUni/server/src/routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userController = require("./controllers/userController");
+const categoryController = require("./controllers/categoryController");
 
 router.use("/users", userController);
+router.use(categoryController);
 
 module.exports = router;

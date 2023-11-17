adc0828b6f267066ff5d052207256aac7c9a4a5e
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-17 23:39:28 +0200

    category model updated

--- kolplace - Final React Project SoftUni/server/src/models/Category.js
+++ kolplace - Final React Project SoftUni/server/src/models/Category.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-
-});
+    name: {
+        type: String,
+        required: [true, "Category name is required!"],
+        minLength: [3, "Category name must be at least 3 characters!"],
+        maxLength: [20, "Category name must not be more than 20 characters!"],
+    },
+    categoryImage: {
+        type: String,
+        required: [true, "Category Image is required!"],
+        match: [/^https?:\/\/.+/, "Provide valid image link!"]
+    }
+}, { timestamps: true });
 
 const Category = mongoose.model("Category", categorySchema);
 

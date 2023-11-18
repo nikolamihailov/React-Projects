9221eebd3ebd85b1e47eaeacc8ac7e87b37e5af0
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-18 23:47:25 +0200

    new custom hook bumped

---
+++ kolplace - Final React Project SoftUni/client/src/hooks/usePagination.js
@@ -0,0 +1,26 @@
+import { useCallback, useState } from "react";
+
+const usePagination = () => {
+    const [page, setPage] = useState(1);
+    const [pageCount, setPageCount] = useState(0);
+
+    const nextPage = useCallback(() =>
+        setPage((p) => {
+            if (p === pageCount) return p;
+            return (p += 1);
+        }), [pageCount]);
+
+    const prevPage = useCallback(() =>
+        setPage((p) => {
+            if (p === 1) return p;
+            return (p -= 1);
+        }), []);
+
+    const updatePageCount = useCallback((pC) => setPageCount(pC), []);
+
+    return {
+        page, pageCount, nextPage, prevPage, updatePageCount
+    };
+};
+
+export default usePagination;

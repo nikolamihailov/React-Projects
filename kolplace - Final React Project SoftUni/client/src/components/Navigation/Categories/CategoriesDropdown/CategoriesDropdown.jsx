00a4bcc2d491f4766a182b4f0fd43cedc1e36311
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-18 23:53:13 +0200

    updated main nav categories dropdown

--- kolplace - Final React Project SoftUni/client/src/components/Navigation/Categories/CategoriesDropdown/CategoriesDropdown.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/Categories/CategoriesDropdown/CategoriesDropdown.jsx
@@ -1,31 +1,13 @@
 import styles from "./Dropdown.module.css";
-const CategoriesDropdown = () => {
+const CategoriesDropdown = ({ categories }) => {
   return (
     <ul className={styles["categories-dropdown"]}>
-      <li>
-        <i className="fa-solid fa-gamepad"></i>
-        <span>Games</span>
-      </li>
-      <li>
-        <i className="fa-solid fa-mobile-screen"></i>
-        <span>Smartphones</span>
-      </li>
-      <li>
-        <i className="fa-solid fa-laptop"></i>
-        <span>Laptops</span>
-      </li>
-      <li>
-        <i className="fa-solid fa-tv"></i>
-        <span> TVs</span>
-      </li>
-      <li>
-        <i className="fa-solid fa-book"></i>
-        <span>Books</span>
-      </li>
-      <li>
-        <i className="fa-solid fa-basketball"></i>
-        <span>Sports</span>
-      </li>
+      {categories.map((c) => (
+        <li key={c._id}>
+          <span>{c.name}</span>
+          <img src={c.categoryImage} alt={c.name} />
+        </li>
+      ))}
     </ul>
   );
 };

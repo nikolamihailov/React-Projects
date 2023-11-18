c60b4e326e817312684ec11dd799dcef61d1f543
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-18 23:45:31 +0200

    removed strict mode for testing

--- kolplace - Final React Project SoftUni/client/src/main.jsx
+++ kolplace - Final React Project SoftUni/client/src/main.jsx
@@ -1,12 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </BrowserRouter>
 );

8168b296fec67844c7186c7a5f82b3a5d0cce5d0
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-18 23:47:11 +0200

    pagination, edit and getOne added

--- kolplace - Final React Project SoftUni/server/src/controllers/categoryController.js
+++ kolplace - Final React Project SoftUni/server/src/controllers/categoryController.js
@@ -3,25 +3,60 @@ const { isAdmin } = require("../middlewares/isAdmin");
 const categoryService = require("../services/categoryService");
 const { extractErrors } = require("../utils/errParse");
 
+const ITEMS_PER_PAGE = 3;
+
 categoryController.get("/categories", async (req, res) => {
     try {
-        const allCategories = await categoryService.getAllCategories();
         if (req.decToken) {
             return res.status(401).json({ expMessage: "Your session has expired, you have to login again!" });
         }
-        res.status(200).json(allCategories);
+        if (req.query.page) {
+            const { page } = req.query;
+            const data = await categoryService.getAllWithPagination(ITEMS_PER_PAGE, page);
+            res.status(200).json(data);
+        } else {
+            const allCategories = await categoryService.getAllCategories();
+            res.status(200).json(allCategories);
+        }
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 });
 
-categoryController.post("/categories", isAdmin, async (req, res) => {
+categoryController.get("/categories/:id", isAdmin, async (req, res) => {
     try {
-        const newCategory = await categoryService.createCategory({ ...req.body });
         if (req.decToken) {
             return res.status(401).json({ expMessage: "Your session has expired, you have to login again!" });
         }
+        const category = await categoryService.getOneCategory(req.params.id);
+        res.status(200).json(category);
+    } catch (error) {
+        const errors = extractErrors(error);
+        res.status(400).json({ errors });
+    }
+});
+
+categoryController.post("/categories", isAdmin, async (req, res) => {
+    try {
+        if (req.decToken) {
+            return res.status(401).json({ expMessage: "Your session has expired, you have to login again!" });
+        }
+        const newCategory = await categoryService.createCategory({ ...req.body });
         res.status(201).json(newCategory);
+    } catch (error) {
+        const errors = extractErrors(error);
+        res.status(400).json({ errors });
+    }
+});
+
+categoryController.put("/categories/:id", isAdmin, async (req, res) => {
+    try {
+        if (req.decToken) {
+            return res.status(401).json({ expMessage: "Your session has expired, you have to login again!" });
+        }
+        const updatedCategory = await categoryService.editCategory(req.params.id, { ...req.body });
+        console.log(updatedCategory);
+        res.status(201).json(updatedCategory);
     } catch (error) {
         const errors = extractErrors(error);
         res.status(400).json({ errors });

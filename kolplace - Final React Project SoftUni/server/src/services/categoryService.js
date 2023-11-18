577e846755a908eb21aa4e3e9a3e9833b3ce6468
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-18 23:46:47 +0200

    new functions for edit and pagination added

--- kolplace - Final React Project SoftUni/server/src/services/categoryService.js
+++ kolplace - Final React Project SoftUni/server/src/services/categoryService.js
@@ -2,4 +2,22 @@ const Category = require("../models/Category");
 
 exports.getAllCategories = () => Category.find();
 
+exports.getAllWithPagination = async (itemsPerPage = 3, page) => {
+
+    const categoryCount = await Category.estimatedDocumentCount();
+
+    const pageCount = Math.ceil(categoryCount / itemsPerPage);
+    const skip = itemsPerPage * (page - 1);
+
+    const categories = await Category.find().skip(skip).limit(itemsPerPage);
+
+    return { categories, pageCount };
+
+};
+exports.getOneCategory = (id) => Category.findById(id);
+
 exports.createCategory = (data) => Category.create(data);
+
+exports.editCategory = (id, data) => Category.findByIdAndUpdate(id, data, { runValidators: true });
+
+

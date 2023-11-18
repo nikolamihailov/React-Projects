d09c306344477a7d5aeb17584cb2a408bd66300d
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-18 23:46:17 +0200

    updated custom hook

--- kolplace - Final React Project SoftUni/client/src/hooks/useShowHide.js
+++ kolplace - Final React Project SoftUni/client/src/hooks/useShowHide.js
@@ -1,12 +1,14 @@
 import { useCallback, useState } from "react";
 
-export const useDropdown = (initialState = false) => {
+export const useShowHide = (initialState = false) => {
     const [isOpen, setIsOpen] = useState(initialState);
     const mouseEnter = useCallback(() => setIsOpen(true), []);
     const mouseLeave = useCallback(() => setIsOpen(false), []);
+    const showHide = useCallback(() => setIsOpen(state => !state), []);
     return {
         isOpen,
         mouseEnter,
-        mouseLeave
+        mouseLeave,
+        showHide
     };
 };

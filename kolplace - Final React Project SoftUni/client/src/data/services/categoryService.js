8ccabae9238c4802e2e5365d0605b5763fef11a1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-18 23:47:36 +0200

    updated

--- kolplace - Final React Project SoftUni/client/src/data/services/categoryService.js
+++ kolplace - Final React Project SoftUni/client/src/data/services/categoryService.js
@@ -3,14 +3,26 @@ import { request } from "../requester";
 
 const categoryEndpoints = {
     getAll: CATEGORIES_URL + "",
+    getOne: CATEGORIES_URL + "/",
     createCategory: CATEGORIES_URL + "",
+    editCategory: CATEGORIES_URL + "/"
 };
 
 export const getAll = async () => {
     return await request.get(categoryEndpoints.getAll);
 };
 
+export const getOneCategory = async (id) => {
+    return await request.get(categoryEndpoints.getOne + id);
+};
+
 export const createCategory = async (data) => {
     return await request.post(categoryEndpoints.createCategory, data);
 };
+
+
+export const editCategory = async (id, data) => {
+    return await request.put(categoryEndpoints.editCategory + id, data);
+};
+
 

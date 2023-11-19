ffa9b52135e0eb6469377d6f74002530c5dc5f1a
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-19 19:45:27 +0200

    updated

--- kolplace - Final React Project SoftUni/client/src/components/Admin/Categories/CategoryItem/CategoryItem.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Categories/CategoryItem/CategoryItem.jsx
@@ -1,18 +1,18 @@
 import styles from "./CategoryItem.module.css";
 
-const CategoryItem = ({ _id, name, categoryImage, openEdit }) => {
+const CategoryItem = ({ _id, name, categoryImage, openEdit, openDelete }) => {
   return (
     <div className={styles["admin-category-item"]}>
-      <h2>{name}</h2>
-      <img src={categoryImage} alt={name} />
       <div>
-        <button onClick={() => openEdit(_id)}>
+        <button onClick={() => openEdit(_id)} title={"Edit Item"}>
           <i className="fa-solid fa-pen-to-square"></i>
         </button>
-        <button>
+        <button onClick={() => openDelete(_id)} title={"Delete Item"}>
           <i className="fa-solid fa-trash-can"></i>
         </button>
       </div>
+      <img src={categoryImage} alt={name} />
+      <h2>{name}</h2>
     </div>
   );
 };

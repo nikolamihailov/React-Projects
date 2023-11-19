a37fa33b69a3747160d3b1591998733bf8149464
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-19 19:43:30 +0200

    updated pages structure and functionality

--- kolplace - Final React Project SoftUni/client/src/components/Pagination/Pagination.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Pagination/Pagination.jsx
@@ -1,13 +1,29 @@
 import styles from "./Pagination.module.css";
 
-const Pagination = ({ page, pageCount, prevPage, nextPage }) => {
+const Pagination = ({ page, pageCount, prevPage, nextPage, switchToPage }) => {
+  const pages = Array.from({ length: pageCount }, (_, idx) => idx + 1);
   return (
     <div className={styles["pagination"]}>
-      <button disabled={page === 1} onClick={prevPage}>
+      <button disabled={page === 1} onClick={prevPage} title={"Previous Page"}>
         <i className="fa-solid fa-angles-left"></i>
       </button>
-      <p>{page}</p>
-      <button disabled={page === pageCount} onClick={nextPage}>
+      {pages.map((p) => {
+        return (
+          <button
+            key={p}
+            onClick={() => switchToPage(p)}
+            className={page === p && styles["page-active"]}
+            title={`Page: ${p}`}
+          >
+            {p}
+          </button>
+        );
+      })}
+      <button
+        disabled={page === pageCount}
+        onClick={nextPage}
+        title={"Next Page"}
+      >
         <i className="fa-solid fa-angles-right"></i>
       </button>
     </div>

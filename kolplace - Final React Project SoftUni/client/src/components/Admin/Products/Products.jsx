12526a94c4f3f8ad722668645e0322409e12a6b9
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-20 23:40:07 +0200

    basic component structure added

--- kolplace - Final React Project SoftUni/client/src/components/Admin/Products/Products.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Products/Products.jsx
@@ -1,9 +1,31 @@
+import { useCallback, useEffect, useState } from "react";
 import styles from "./Products.module.css";
+import AddProductItem from "./AddProductItem/AddProduct";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [isAddOpen, setIsAddOpen] = useState(false);
+
+  useEffect(() => {
+    setProducts(["op"]);
+  }, []);
+
+  const onCloseAdd = useCallback(() => setIsAddOpen(false), []);
+
   return (
     <div className={styles["admin-products"]}>
       <h1>All Products</h1>
+
+      <div className={styles["admin-products-top"]}>
+        <button onClick={() => setIsAddOpen(true)}>
+          Add product <i className="fa-solid fa-circle-plus"></i>
+        </button>
+      </div>
+
+      <div className={styles["admin-products-container"]}>
+        {products?.length > 0 ? <h3>Opa</h3> : <h2>No products</h2>}
+      </div>
+      {isAddOpen && <AddProductItem onClose={onCloseAdd} />}
     </div>
   );
 };

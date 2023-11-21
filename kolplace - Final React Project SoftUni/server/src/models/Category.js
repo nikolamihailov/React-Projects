1d319bbe8b25fbf19fc1a5b4908bc06b4e783d20
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-21 23:53:23 +0200

    category model updated

--- kolplace - Final React Project SoftUni/server/src/models/Category.js
+++ kolplace - Final React Project SoftUni/server/src/models/Category.js
@@ -6,6 +6,15 @@ const categorySchema = new mongoose.Schema({
         required: [true, "Category name is required!"],
         minLength: [3, "Category name must be at least 3 characters!"],
         maxLength: [20, "Category name must not be more than 20 characters!"],
+        unique: true,
+        validator: async function (name) {
+            if (this.isModified('name') || this.isNew) {
+                const category = await Category.findOne({ name });
+                return !category;
+            }
+            return true;
+        },
+        message: 'Category with this name already exists!',
     },
     categoryImage: {
         type: String,

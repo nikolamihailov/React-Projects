d6966a221e3075e6242ca081d767bf18fbf2e9fe
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-21 18:29:06 +0200

    create product added

--- kolplace - Final React Project SoftUni/server/src/services/productService.js
+++ kolplace - Final React Project SoftUni/server/src/services/productService.js
@@ -1,3 +1,7 @@
 
 // !!!!!!!!!!!!!!!!!!
 // when using do not forget - const populatedProduct = await Product.findById(productId).populate('reviews').populate('category');
+
+const Product = require("../models/Product");
+
+exports.addProduct = (data) => Product.create(data);

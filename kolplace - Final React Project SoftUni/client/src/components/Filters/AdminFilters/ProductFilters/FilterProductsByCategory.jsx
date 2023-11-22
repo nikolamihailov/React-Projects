52f570499d43881254ca717e4702d8c9fc920f3e
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-22 14:51:19 +0200

    fixed error

--- kolplace - Final React Project SoftUni/client/src/components/Filters/AdminFilters/ProductFilters/FilterProductsByCategory.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Filters/AdminFilters/ProductFilters/FilterProductsByCategory.jsx
@@ -1,18 +1,32 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import styles from "../../Filters.module.css";
 import { getAll } from "../../../../data/services/categoryService";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../../contexts/AuthContext";
+import { NotifContext } from "../../../../contexts/NotificationContext";
 
 const FilterProductsByCategory = ({ onChange }) => {
   const [categories, setCategories] = useState([]);
+  const { updateAuth } = useContext(AuthContext);
+  const navigateTo = useNavigate();
+  const { updateNotifs } = useContext(NotifContext);
 
   useEffect(() => {
     getAll()
-      .then((data) => setCategories(data))
+      .then((data) => {
+        if (data.expMessage) {
+          updateNotifs([{ text: data.expMessage, type: "error" }]);
+          navigateTo("/login");
+          updateAuth({});
+        }
+        setCategories(data);
+      })
       .catch((err) => console.log(err));
-  });
+  }, [updateNotifs, navigateTo, updateAuth]);
+
   return (
     <div className={styles["filters"]}>
-      <select name="filters" id="filters" onChange={onChange}>
+      <select name="filtersCategory" id="filtersCategory" onChange={onChange}>
         <option value="all">All</option>
         {categories?.map((c) => {
           return (
--- kolplace - Final React Project SoftUni/client/src/components/Navigation/Categories/Categories.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/Categories/Categories.jsx
@@ -2,19 +2,30 @@ import styles from "./Categories.module.css";
 import CategoriesDropdown from "./CategoriesDropdown/CategoriesDropdown";
 import { useShowHide } from "../../../hooks/useShowHide";
 import { getAll } from "../../../data/services/categoryService";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { NotifContext } from "../../../contexts/NotificationContext";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
   const { isOpen, mouseEnter, mouseLeave } = useShowHide();
+  const { updateAuth } = useContext(AuthContext);
+  const navigateTo = useNavigate();
+  const { updateNotifs } = useContext(NotifContext);
 
   useEffect(() => {
     getAll()
       .then((data) => {
+        if (data.expMessage) {
+          updateNotifs([{ text: data.expMessage, type: "error" }]);
+          navigateTo("/login");
+          updateAuth({});
+        }
         setCategories(data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [navigateTo, updateAuth, updateNotifs]);
 
   return (
     <div

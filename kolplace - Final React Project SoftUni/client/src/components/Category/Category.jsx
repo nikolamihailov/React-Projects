19e958c6d01923a8a7c96382de882f153c9377b9
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-23 18:22:00 +0200

    category section updated with products and info

--- kolplace - Final React Project SoftUni/client/src/components/Category/Category.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Category/Category.jsx
@@ -1,25 +1,76 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { getOneCategoryByName } from "../../data/services/categoryService";
+import { NotifContext } from "../../contexts/NotificationContext";
+import { AuthContext } from "../../contexts/AuthContext";
+import { getAllFromCategory } from "../../data/services/productService";
+import ProductItem from "./ProductItem/ProductItem";
+
+import styles from "./Category.module.css";
 
 const Category = () => {
   const { name } = useParams();
-  const [category, setCategory] = useState();
+  const [category, setCategory] = useState(null);
+  const [products, setProducts] = useState([]);
+  const navigateTo = useNavigate();
+  const { updateNotifs } = useContext(NotifContext);
+  const { updateAuth } = useContext(AuthContext);
 
   useEffect(() => {
-    getOneCategoryByName(name)
-      .then((data) => {
-        setCategory(data);
-      })
-      .catch((err) => console.log(err));
-  }, [name]);
+    const fetchData = async () => {
+      try {
+        const [categoryData, productsData] = await Promise.all([
+          getOneCategoryByName(name),
+          getAllFromCategory(name),
+        ]);
+
+        if (categoryData.expMessage) {
+          updateNotifs([{ text: categoryData.expMessage, type: "error" }]);
+          navigateTo("/login");
+          updateAuth({});
+        }
+        if (categoryData.nonExisting) {
+          updateNotifs([{ text: categoryData.nonExisting, type: "error" }]);
+          navigateTo("/error");
+        }
+        setCategory(categoryData);
+
+        if (productsData.expMessage) {
+          updateNotifs([{ text: productsData.expMessage, type: "error" }]);
+          navigateTo("/login");
+          updateAuth({});
+        }
+        setProducts(productsData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, [name, updateAuth, updateNotifs, navigateTo]);
 
   return (
-    <div>
-      <h1>{name}</h1>
-      <img src={category?.categoryImage} />
-      <p>Hello there!</p>
-    </div>
+    <section className={styles["category-section"]}>
+      <div>
+        <h1>{category?.name}</h1>
+        <img
+          className={styles["category-banner-image"]}
+          src={category?.categoryImage}
+          alt={category?.name}
+        />
+        <p>Hello there!</p>
+      </div>
+
+      <div className={styles["products"]}>
+        {products.length > 0 ? (
+          products.map((p) => {
+            return <ProductItem key={p._id} {...p} />;
+          })
+        ) : (
+          <h2>No products in this category!</h2>
+        )}
+      </div>
+    </section>
   );
 };
 

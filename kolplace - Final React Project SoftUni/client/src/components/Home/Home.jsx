e704e8e273693ea9b122c004f46ecccea2be2e99
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-23 18:33:10 +0200

    features added to home

--- kolplace - Final React Project SoftUni/client/src/components/Home/Home.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import useTitle from "../../hooks/useTitle";
 import Carousel from "./CategoryCarousel/CarouselContainer/Carousel";
 import styles from "./Home.module.css";
+import ShopFeaturesSection from "./ShopFeatures/ShopFeatures";
 
 const Home = () => {
   useTitle("KolPlace - Your Shopping Place");
   return (
     <section className={styles["home"]}>
       <Carousel />
+      <ShopFeaturesSection />
     </section>
   );
 };

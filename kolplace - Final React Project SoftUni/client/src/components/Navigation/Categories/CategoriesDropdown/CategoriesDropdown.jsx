47f09c191d7aa1c7a1e1f0410c89be3132eafb94
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-23 02:34:51 +0200

    links added to dropdown li

--- kolplace - Final React Project SoftUni/client/src/components/Navigation/Categories/CategoriesDropdown/CategoriesDropdown.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/Categories/CategoriesDropdown/CategoriesDropdown.jsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
 import styles from "./Dropdown.module.css";
-const CategoriesDropdown = ({ categories }) => {
+const CategoriesDropdown = ({ categories, onClose }) => {
   return (
     <ul className={styles["categories-dropdown"]}>
       {categories.map((c) => (
-        <li key={c._id}>
-          <span>{c.name}</span>
-          <img src={c.categoryImage} alt={c.name} />
-        </li>
+        <Link
+          to={`/categories/${c.name.toLowerCase()}`}
+          key={c._id}
+          onClick={onClose}
+        >
+          <li>
+            <span>{c.name}</span>
+            <img src={c.categoryImage} alt={c.name} />
+          </li>
+        </Link>
       ))}
     </ul>
   );

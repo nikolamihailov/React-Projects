d0104ecf202110aa3bd4e6191a45d21e750e36ae
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-23 18:21:08 +0200

    map changed to different location

--- kolplace - Final React Project SoftUni/client/src/components/About/About.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/About/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
       </div>
       <iframe
         title="Google Maps"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317716.60639487504!2d-0.43123851645035693!3d51.528607021344676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2z0JvQvtC90LTQvtC9LCDQktC10LvQuNC60L7QsdGA0LjRgtCw0L3QuNGP!5e0!3m2!1sbg!2sbg!4v1700518163251!5m2!1sbg!2sbg"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7220.342447010138!2d55.27434804195275!3d25.19744761881899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f682829c85c07%3A0xa5eda9fb3c93b69d!2z0JTRg9Cx0LDQuSDQnNC-0Ls!5e0!3m2!1sbg!2sbg!4v1700751018522!5m2!1sbg!2sbg"
         allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>

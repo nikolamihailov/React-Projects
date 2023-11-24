c1485ad48324c41b7288dcfea63145801e34d11f
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-24 22:03:35 +0200

    simple about section bumped

---
+++ kolplace - Final React Project SoftUni/client/src/components/Home/AboutSection/ShortAbout.jsx
@@ -0,0 +1,21 @@
+import styles from "../Home.module.css";
+
+const ShortAboutSection = () => {
+  return (
+    <div className={styles["short-about-section"]}>
+      <h3>Online Store KolPlace</h3>
+      <p>
+        Welcome to KolPlace - Your Ultimate Online Shopping Destination! Since
+        2020, KolPlace has been your go-to platform for endless entertainment.
+        From games and electronics to books, puzzles, and more, we offer diverse
+        products with top-notch service and fast delivery. Join us for a dynamic
+        shopping experience, where we continually enrich our catalog to meet
+        your evolving needs. Whether you&apos;re a gamer, reader, or tech
+        enthusiast, KolPlace is the preferred choice for all people seeking
+        convenience, variety, and unbeatable service.
+      </p>
+    </div>
+  );
+};
+
+export default ShortAboutSection;

5d227c9c1833f1aac124b6fab5b8f7cf6c5b386b
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-24 22:04:03 +0200

    added carousels and about to home and styled them

--- kolplace - Final React Project SoftUni/client/src/components/Home/Home.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import useTitle from "../../hooks/useTitle";
+import ShortAboutSection from "./AboutSection/ShortAbout";
 import Carousel from "./CategoryCarousel/CarouselContainer/Carousel";
 import styles from "./Home.module.css";
+import CarouselProducts from "./ProductsCarousel/CarouselContainer/Carousel";
 import ShopFeaturesSection from "./ShopFeatures/ShopFeatures";
 
 const Home = () => {
@@ -9,6 +11,14 @@ const Home = () => {
     <section className={styles["home"]}>
       <Carousel />
       <ShopFeaturesSection />
+      <CarouselProducts category={"games"} items={5} title={"Latest Games"} />
+      <CarouselProducts
+        category={"smartphones"}
+        items={5}
+        title={"Latest Smartphones"}
+      />
+      <CarouselProducts category={"books"} items={5} title={"Latest Books"} />
+      <ShortAboutSection />
     </section>
   );
 };

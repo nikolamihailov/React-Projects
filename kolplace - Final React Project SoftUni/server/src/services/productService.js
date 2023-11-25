080d67f1573c673e91138d24d68a3146fa98c2bd
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-25 14:41:50 +0200

    altered product service to work with promoproducts

--- kolplace - Final React Project SoftUni/server/src/services/productService.js
+++ kolplace - Final React Project SoftUni/server/src/services/productService.js
@@ -7,13 +7,14 @@ const Product = require("../models/Product");
 
 exports.getAllProducts = () => Product.find().populate("category");
 
-exports.getAllWithFilters = async (itemsPerPage = 8, page, filter = "", category) => {
+exports.getAllWithFilters = async (itemsPerPage = 8, page, filter = "", category, onPromotion) => {
     let order = "";
     const query = {};
     if (filter) {
         if (filter.includes("name")) query.name = filter.split("-")[1];
         if (filter.includes("createdAt")) query.createdAt = filter.split("-")[1];
         if (filter.includes("price")) query.price = filter.split("-")[1];
+        if (filter.includes("price") && onPromotion) query.promoPrice = filter.split("-")[1];
         order = filter.split("-")[1];
     }
 
@@ -22,6 +23,27 @@ exports.getAllWithFilters = async (itemsPerPage = 8, page, filter = "", category
 
     if (category) findQuery.category = category;
     if (category === "all") findQuery = {};
+
+    // console.log(typeof onPromotion);
+
+    if (onPromotion === "true") {
+        findQuery.hasPromoPrice = true;
+        if (Object.keys(query).length > 0) {
+            products = await Product.find(findQuery).sort(query).collation({ locale: 'en', strength: 2 }).populate("category");
+        } else products = await Product.find(findQuery).populate("category");
+
+        const productsCount = products.length;
+
+        let pageCount = Math.ceil(productsCount / itemsPerPage);
+        let skip = itemsPerPage * (page - 1);
+        if (productsCount <= itemsPerPage) {
+            skip = 0;
+            pageCount = 1;
+        }
+
+        products = products.slice(skip, skip + itemsPerPage);
+        return { products, pageCount, productsCount };
+    }
 
     /* if (Object.keys(query).length > 0) products = await Product.find(findQuery).sort(query).collation({ locale: 'en', strength: 2 });*/
     if (Object.keys(query).length > 0) {

f44748f619ea021332d0ce0cea49981f66836040
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-26 23:13:13 +0200

    routes updated

--- kolplace - Final React Project SoftUni/server/src/routes.js
+++ kolplace - Final React Project SoftUni/server/src/routes.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const userController = require("./controllers/userController");
 const categoryController = require("./controllers/categoryController");
 const productController = require("./controllers/productController");
+const shoppingCartController = require("./controllers/shoppingCartController");
+
 
 router.use("/users", userController);
 router.use(productController);
 router.use(categoryController);
+router.use(shoppingCartController);
 
 module.exports = router;

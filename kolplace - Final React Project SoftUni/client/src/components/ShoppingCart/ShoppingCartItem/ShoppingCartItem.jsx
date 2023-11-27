74cef334d0173d04c314d057287e4dd4e60c6f57
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-27 23:30:19 +0200

    remove product added to cartitem

--- kolplace - Final React Project SoftUni/client/src/components/ShoppingCart/ShoppingCartItem/ShoppingCartItem.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/ShoppingCart/ShoppingCartItem/ShoppingCartItem.jsx
@@ -1,12 +1,19 @@
 import styles from "./ShoppingCartItem.module.css";
 
-const ShoppingCartItem = ({ name, mainImage, price }) => {
+const ShoppingCartItem = ({
+  _id,
+  name,
+  mainImage,
+  price,
+  removeCartProduct,
+}) => {
   return (
-    <div className={styles["cart-item-container"]}>
-      <div className={styles["cart-item"]}>
-        <img src={mainImage} alt={name} />
-        <h4>{name}</h4>
-        <p>${price}</p>
+    <div className={styles["cart-item"]}>
+      <img src={mainImage} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p>
+      <div className={styles["remove"]} onClick={() => removeCartProduct(_id)}>
+        <i className="fa-solid fa-trash-can"></i>
       </div>
     </div>
   );

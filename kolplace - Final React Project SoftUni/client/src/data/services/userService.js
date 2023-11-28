de3409e0d61aad2c78c612f638b99ce25d23e0d6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-29 00:30:16 +0200

    new function bumped

--- kolplace - Final React Project SoftUni/client/src/data/services/userService.js
+++ kolplace - Final React Project SoftUni/client/src/data/services/userService.js
@@ -7,6 +7,7 @@ const userEndpoints = {
     logout: USERS_URL + "/logout",
     getFavouriteProducts: USERS_URL + "/",
     addProductToFavourites: USERS_URL + "/",
+    removeProductFromFavourites: USERS_URL + "/",
 };
 
 export const register = async (data) => {
@@ -18,11 +19,13 @@ export const login = async (data) => {
 };
 
 export const getFavouriteProducts = async (userId) => {
-    console.log(userEndpoints.getFavouriteProducts + userId);
     return await request.get(userEndpoints.getFavouriteProducts + userId);
 };
 
 export const addProductToFavourites = async (userId, data) => {
     return await request.post(userEndpoints.addProductToFavourites + userId, data);
+};
 
+export const removeProductFromFavourites = async (userId, data) => {
+    return await request.put(userEndpoints.removeProductFromFavourites + userId, data);
 };

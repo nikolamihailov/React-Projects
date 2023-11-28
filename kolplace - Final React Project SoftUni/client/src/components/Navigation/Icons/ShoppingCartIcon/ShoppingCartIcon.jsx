0e9363b55799f5e2a1fc64c12ce1f63fc21590ad
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-28 22:06:47 +0200

    guard added for guests

--- kolplace - Final React Project SoftUni/client/src/components/Navigation/Icons/ShoppingCartIcon/ShoppingCartIcon.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/Icons/ShoppingCartIcon/ShoppingCartIcon.jsx
@@ -1,33 +1,46 @@
-import { useContext, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 import ShoppingCartExpanded from "../../../ShoppingCart/ShoppingCartExpanded/ShoppingCartAside";
 import styles from "../Icons.module.css";
 import { AuthContext } from "../../../../contexts/AuthContext";
 import { ShoppingCartContext } from "../../../../contexts/ShoppingCartContext";
+import { NotifContext } from "../../../../contexts/NotificationContext";
+import { useNavigate } from "react-router-dom";
 
 const ShoppingCartIcon = () => {
   const [isOpen, setIOpen] = useState(false);
   const { isAuthenticated } = useContext(AuthContext);
   const { cart } = useContext(ShoppingCartContext);
+  const { updateNotifs } = useContext(NotifContext);
+  const navigateTo = useNavigate();
 
-  const onClick = () => {
-    if (isAuthenticated) setIOpen(!isOpen);
-  };
+  const onOpen = useCallback(() => setIOpen(!isOpen), [isOpen]);
+
   return (
     <>
       <div className={styles["dropdown-container"]}>
         <button
           className={styles["icon-btn"]}
           title="Shopping Cart"
-          onClick={onClick}
+          onClick={() => {
+            if (isAuthenticated) {
+              setIOpen(!isOpen);
+            } else {
+              updateNotifs([
+                {
+                  text: "You need to be signed in to view shopping cart!",
+                  type: "error",
+                },
+              ]);
+              navigateTo("/login");
+            }
+          }}
         >
           <i className="fa-solid fa-cart-shopping"></i>
           {isAuthenticated && (
             <span>{cart?.products.length > 0 ? cart?.products.length : 0}</span>
           )}
         </button>
-        {isAuthenticated && isOpen && (
-          <ShoppingCartExpanded onClick={onClick} />
-        )}
+        {isAuthenticated && isOpen && <ShoppingCartExpanded onClick={onOpen} />}
       </div>
     </>
   );

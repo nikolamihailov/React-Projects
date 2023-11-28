08405bd68e9d0f3725f429b3cf02f881155fe1f1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-28 20:37:33 +0200

    favourite products page started

---
+++ kolplace - Final React Project SoftUni/client/src/components/FavouriteProducts/FavouriteProducts.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { FavouriteProductsContext } from "../../contexts/FavouriteProducts";
+
+const FavouriteProducts = () => {
+  const { favProducts } = useContext(FavouriteProductsContext);
+  return (
+    <section>
+      <h1>Favourite Products</h1>
+      <div>{console.log(favProducts)}</div>
+    </section>
+  );
+};
+
+export default FavouriteProducts;

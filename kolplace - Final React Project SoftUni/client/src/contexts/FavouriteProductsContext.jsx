7207b9c741bc25fe930323d76ece9419426b8cd5
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-28 20:35:49 +0200

    favourite products context with reducer added

---
+++ kolplace - Final React Project SoftUni/client/src/contexts/FavouriteProductsContext.jsx
@@ -0,0 +1,76 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
+import { AuthContext } from "./AuthContext";
+import {
+  addProductToFavourites,
+  getFavouriteProducts,
+} from "../data/services/userService";
+
+export const FavouriteProductsContext = createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INIT":
+      return {
+        ...state,
+        favouriteProducts: action.products,
+      };
+    case "ADD_PRODUCT":
+      return {
+        ...state,
+        favouriteProducts: action.products,
+      };
+    default:
+      return state;
+  }
+};
+export const FavouriteProductsProvider = ({ children }) => {
+  const { auth } = useContext(AuthContext);
+  const [favProducts, dispatch] = useReducer(reducer, {
+    favouriteProducts: [],
+  });
+  // console.log(auth.user._id);
+
+  useEffect(() => {
+    getFavouriteProducts(auth.user?._id)
+      .then((data) => {
+        console.log(data);
+        dispatch({
+          type: "INIT",
+          products: data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, [auth.user?._id]);
+
+  const addProductToFavouriteProducts = useCallback(
+    async (productId) => {
+      console.log(productId);
+      const updated = await addProductToFavourites(auth.user._id, {
+        productId,
+      });
+      console.log(updated);
+      dispatch({
+        type: "ADD_PRODUCT",
+        products: updated,
+      });
+    },
+    [auth.user._id]
+  );
+
+  const ctxValues = {
+    favProducts,
+    addProductToFavouriteProducts,
+  };
+
+  return (
+    <FavouriteProductsContext.Provider value={ctxValues}>
+      {children}
+    </FavouriteProductsContext.Provider>
+  );
+};

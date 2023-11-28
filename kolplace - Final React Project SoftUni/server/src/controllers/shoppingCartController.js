931d434bc1854ad67d5b98de2d081a63e69c4474
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-29 00:59:13 +0200

    guard used for shopping cart

--- kolplace - Final React Project SoftUni/server/src/controllers/shoppingCartController.js
+++ kolplace - Final React Project SoftUni/server/src/controllers/shoppingCartController.js
@@ -1,7 +1,10 @@
 const shoppingCartController = require("express").Router();
 const shoppingCartService = require("../services/shoppingCartService");
+const { isAuthenticated } = require("../middlewares/isAuthenticated");
 
-shoppingCartController.get("/shopping-cart/:id", async (req, res) => {
+
+
+shoppingCartController.get("/shopping-cart/:id", isAuthenticated, async (req, res) => {
     try {
         const cart = await shoppingCartService.getById(req.params.id);
         res.status(200).json(cart);
@@ -10,7 +13,7 @@ shoppingCartController.get("/shopping-cart/:id", async (req, res) => {
     }
 });
 
-shoppingCartController.post("/shopping-cart/:id", async (req, res) => {
+shoppingCartController.post("/shopping-cart/:id", isAuthenticated, async (req, res) => {
     try {
         const updatedCart = await shoppingCartService.addToCart(req.params.id, req.body);
         res.status(200).json(updatedCart);
@@ -19,7 +22,7 @@ shoppingCartController.post("/shopping-cart/:id", async (req, res) => {
     }
 });
 
-shoppingCartController.put("/shopping-cart/:id", async (req, res) => {
+shoppingCartController.put("/shopping-cart/:id", isAuthenticated, async (req, res) => {
     try {
         console.log(req.body);
         const updatedCart = await shoppingCartService.removeProduct(req.params.id, req.body);
@@ -29,7 +32,7 @@ shoppingCartController.put("/shopping-cart/:id", async (req, res) => {
     }
 });
 
-shoppingCartController.delete("/shopping-cart/:id", async (req, res) => {
+shoppingCartController.delete("/shopping-cart/:id", isAuthenticated, async (req, res) => {
     try {
         const updatedCart = await shoppingCartService.emptyCart(req.params.id);
         res.status(200).json(updatedCart);

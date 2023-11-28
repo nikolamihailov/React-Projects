f0f6c4daf397690e1d0d0a1c174edda3f83060c2
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-28 22:06:08 +0200

    favourite products count and guard added

--- kolplace - Final React Project SoftUni/client/src/components/Navigation/Icons/FavouritesIcon/FavouritesIcon.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/Icons/FavouritesIcon/FavouritesIcon.jsx
@@ -1,15 +1,37 @@
 import { useContext } from "react";
 import styles from "../Icons.module.css";
 import { AuthContext } from "../../../../contexts/AuthContext";
+import { FavouriteProductsContext } from "../../../../contexts/FavouriteProductsContext";
+import { useNavigate } from "react-router-dom";
+import { NotifContext } from "../../../../contexts/NotificationContext";
 
 const FavouritesIcon = () => {
   const { isAuthenticated } = useContext(AuthContext);
+  const { favProducts } = useContext(FavouriteProductsContext);
+  const { updateNotifs } = useContext(NotifContext);
+  const navigateTo = useNavigate();
 
   return (
     <>
-      <button className={styles["icon-btn"]} title="Favourite Products">
+      <button
+        className={styles["icon-btn"]}
+        title="Favourite Products"
+        onClick={() => {
+          if (isAuthenticated) {
+            navigateTo("/favourite-products");
+          } else {
+            updateNotifs([
+              {
+                text: "You need to be signed in to view favourite products!",
+                type: "error",
+              },
+            ]);
+            navigateTo("/login");
+          }
+        }}
+      >
         <i className="fa-regular fa-heart"></i>
-        {isAuthenticated && <span>0</span>}
+        {isAuthenticated && <span>{favProducts.favouriteProducts.length}</span>}
       </button>
     </>
   );

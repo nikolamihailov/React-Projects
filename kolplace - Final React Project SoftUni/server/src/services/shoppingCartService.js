5eef68cea0211ed9548cd85c58ccdce6af9010c5
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-28 02:05:44 +0200

    service updated with better functionality

--- kolplace - Final React Project SoftUni/server/src/services/shoppingCartService.js
+++ kolplace - Final React Project SoftUni/server/src/services/shoppingCartService.js
@@ -2,11 +2,47 @@ const ShoppingCart = require("../models/ShoppinCart");
 
 exports.getById = (id) => ShoppingCart.findById(id).populate("products.product");
 
-exports.addToCart = (id, product) => ShoppingCart.findByIdAndUpdate(id, {
+/*exports.addToCart = (id, product) => ShoppingCart.findByIdAndUpdate(id, {
     $push: {
         products: product
     }
-}, { new: true, runValidators: true }).populate("products.product");
+}, { new: true, runValidators: true }).populate("products.product");*/
+
+exports.addToCart = async (id, product) => {
+    let cart = await ShoppingCart.findById(id);
+    const hasItem = cart.products.find(p => p.product._id.toString() === product.product);
+    if (hasItem) hasItem.quantity = product.quantity;
+    else {
+        cart.products.push({ product: product.product, quantity: product.quantity });
+    }
+    await cart.save();
+    cart = await ShoppingCart.findById(id).populate("products.product");
+    return cart;
+};
+
+/* exports.addToCart = async (id, product) => {
+    let cart = await ShoppingCart.findById(id).populate("products.product");
+
+    // Find the index of the product in the cart
+    const productIndex = cart.products.findIndex(p => p.product._id.toString() === product.product);
+
+    if (productIndex !== -1) {
+        // If the product exists, update the quantity
+        cart.products[productIndex].quantity = product.quantity;
+    } else {
+        // If the product does not exist, add it to the cart
+        cart.products.push({ product: product.product, quantity: product.quantity });
+    }
+
+    // Save the cart
+    await cart.save();
+
+    // Populate the products before returning
+    cart = await ShoppingCart.findById(id).populate("products.product");
+
+    return cart;
+}; */
+
 
 exports.removeProduct = async (id, product) => {
     const cart = await ShoppingCart.findById(id).populate("products.product");

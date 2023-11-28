36accf022a775d97bd6a827af5dab5cafc41a530
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-11-28 18:18:48 +0200

    user can now add product from category products

--- kolplace - Final React Project SoftUni/client/src/components/Category/ProductItem/ProductItem.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Category/ProductItem/ProductItem.jsx
@@ -1,6 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./ProductItem.module.css";
 import { motion } from "framer-motion";
+import { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { ShoppingCartContext } from "../../../contexts/ShoppingCartContext";
+import { NotifContext } from "../../../contexts/NotificationContext";
 
 const ProductItem = ({
   _id,
@@ -11,7 +15,20 @@ const ProductItem = ({
   promoPrice,
   category,
 }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+  const { cart, addProductToCart } = useContext(ShoppingCartContext);
+  const { updateNotifs } = useContext(NotifContext);
+  const navigateTo = useNavigate();
   const discountInPercentage = Math.ceil(((price - promoPrice) / price) * 100);
+  const product = {
+    _id,
+    name,
+    mainImage,
+    price,
+    hasPromoPrice,
+    promoPrice,
+    category,
+  };
 
   return (
     <motion.div
@@ -52,8 +69,34 @@ const ProductItem = ({
           <p className={styles["promo-price"]}>${promoPrice.toFixed(2)}</p>
         )}
       </div>
-      <button>
-        Buy <i className="fa-solid fa-cart-shopping"></i>{" "}
+      <button
+        onClick={() => {
+          if (isAuthenticated) {
+            addProductToCart(product);
+            const isIn = cart.products.find((p) => p.product._id === _id);
+            if (isIn)
+              updateNotifs([
+                {
+                  text: `Product already in cart, quantity increased!`,
+                  type: "success",
+                },
+              ]);
+            else
+              updateNotifs([
+                { text: `${name} added to cart!`, type: "success" },
+              ]);
+          } else {
+            updateNotifs([
+              {
+                text: "You need to be signed in to buy products!",
+                type: "error",
+              },
+            ]);
+            navigateTo("/login");
+          }
+        }}
+      >
+        Buy <i className="fa-solid fa-cart-shopping"></i>
       </button>
     </motion.div>
   );

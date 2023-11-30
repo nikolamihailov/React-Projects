70c22aa6fec5e57335a7102e8c24d2407a4bb5a1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-01 00:53:13 +0200

    upgraded express config to handle larger files

--- kolplace - Final React Project SoftUni/server/src/config/expressConfig.js
+++ kolplace - Final React Project SoftUni/server/src/config/expressConfig.js
@@ -10,8 +10,10 @@ const routes = require("../routes");
 
 const startServer = () => {
     const app = express();
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
+    /* app.use(express.urlencoded({ extended: false }));
+    app.use(express.json()); */
+    app.use(express.json({ limit: '30mb' }));
+    app.use(express.urlencoded({ extended: false, limit: '30mb' }));
     app.use(cors());
     app.use(auth);
     app.use(routes);

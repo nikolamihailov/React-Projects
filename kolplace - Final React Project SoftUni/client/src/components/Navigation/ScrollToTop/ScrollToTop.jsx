714b7d07e82a143f0acddbec3636eab306dc6ef1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-01 01:50:25 +0200

    scroll to top btn added

---
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, useCallback } from "react";
+import styles from "./ScrollToTop.module.css";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    const scrollvalid = window.scrollY > 700;
+    setIsVisible(scrollvalid);
+  }, []);
+
+  const scrollToTop = useCallback(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  return (
+    <button
+      className={isVisible ? styles["visible"] : styles["hidden"]}
+      onClick={scrollToTop}
+    >
+      <i className="fa-solid fa-arrow-up"></i>
+    </button>
+  );
+};
+
+export default ScrollToTop;

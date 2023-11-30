b821e4b2729e2a5c43a178915587f8741ea007e7
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-01 00:54:02 +0200

    get and update profile added

--- kolplace - Final React Project SoftUni/client/src/data/services/userService.js
+++ kolplace - Final React Project SoftUni/client/src/data/services/userService.js
@@ -5,6 +5,8 @@ const userEndpoints = {
     register: USERS_URL + "/register",
     login: USERS_URL + "/login",
     logout: USERS_URL + "/logout",
+    getUserInfo: USERS_URL + "/",
+    updateUserInfo: USERS_URL + "/",
     getFavouriteProducts: USERS_URL + "/",
     addProductToFavourites: USERS_URL + "/",
     removeProductFromFavourites: USERS_URL + "/",
@@ -18,14 +20,23 @@ export const login = async (data) => {
     return await request.post(userEndpoints.login, data);
 };
 
+export const getProfile = async (userId) => {
+    return await request.get(userEndpoints.getUserInfo + userId);
+};
+
+export const updateProfile = async (userId, data) => {
+    return await request.put(userEndpoints.updateUserInfo + userId, data);
+};
+
+
 export const getFavouriteProducts = async (userId) => {
-    return await request.get(userEndpoints.getFavouriteProducts + userId);
+    return await request.get(userEndpoints.getFavouriteProducts + userId + "/favourites");
 };
 
 export const addProductToFavourites = async (userId, data) => {
-    return await request.post(userEndpoints.addProductToFavourites + userId, data);
+    return await request.post(userEndpoints.addProductToFavourites + userId + "/favourites", data);
 };
 
 export const removeProductFromFavourites = async (userId, data) => {
-    return await request.put(userEndpoints.removeProductFromFavourites + userId, data);
+    return await request.put(userEndpoints.removeProductFromFavourites + userId + "/favourites", data);
 };

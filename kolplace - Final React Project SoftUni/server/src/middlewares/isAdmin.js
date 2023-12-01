47404048d86e964fb1fffd6541735eab5f938bd5
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-01 23:56:20 +0200

    guards refactored

--- kolplace - Final React Project SoftUni/server/src/middlewares/isAdmin.js
+++ kolplace - Final React Project SoftUni/server/src/middlewares/isAdmin.js
@@ -1,5 +1,5 @@
 exports.isAdmin = (req, res, next) => {
-    if (req.user && req.user.data.role === "admin") {
+    if (req.user.data.user && req.user?.data.user.role === "admin") {
         next();
     } else {
         res.status(401).json({ unathorized: "You have no power here!" });
--- kolplace - Final React Project SoftUni/server/src/middlewares/isAuthenticated.js
+++ kolplace - Final React Project SoftUni/server/src/middlewares/isAuthenticated.js
@@ -1,5 +1,5 @@
 exports.isAuthenticated = (req, res, next) => {
-    if (req.user) {
+    if (req.user?.data.user._id) {
         next();
     } else {
         res.status(401).json({ unathorized: "You have no power here!" });

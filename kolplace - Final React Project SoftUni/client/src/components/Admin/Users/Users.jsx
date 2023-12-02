e3585922f109ff4a782fe3f8cbca30f4412f61a4
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-02 17:11:27 +0200

    users refactored

--- kolplace - Final React Project SoftUni/client/src/components/Admin/Users/Users.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Users/Users.jsx
@@ -1,29 +1,53 @@
 import { useCallback, useEffect, useState } from "react";
 import styles from "./Users.module.css";
-import { getAllUsers } from "../../../data/services/userService";
+import { getAllWithFilters } from "../../../data/services/userService";
 import Spinner from "../../Spinner/Spinner";
 import { motion, AnimatePresence } from "framer-motion";
 import UserProfile from "./UserProfile/UserProfile";
+import usePagination from "../../../hooks/usePagination";
+import Pagination from "../../Pagination/Pagination";
+import FilterUsersBySort from "../../Filters/AdminFilters/UserFilter/FilterUsersBySort";
+import DeleteUser from "./DeleteUser/DeleteUser";
 
 const Users = () => {
+  const { page, pageCount, prevPage, nextPage, switchToPage, updatePageCount } =
+    usePagination();
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [deleteOpen, setDeleteOpen] = useState(false);
+  const [sortFilter, setSortFilter] = useState("");
+  const [selectedUser, setSelectedUser] = useState(null);
 
   useEffect(() => {
-    getAllUsers()
+    getAllWithFilters(page, sortFilter)
       .then((data) => {
-        setUsers(data);
+        setUsers(data.users);
+        updatePageCount(data.pageCount);
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
+  }, [updatePageCount, page, sortFilter]);
+
+  const updateUsers = useCallback((data) => setUsers(data), []);
+
+  const openDelete = useCallback((id) => {
+    setDeleteOpen(true);
+    setSelectedUser(id);
   }, []);
 
-  const openDelete = useCallback(() => setDeleteOpen(true), []);
+  const onCloseDelete = useCallback(() => setDeleteOpen(false), []);
+
+  const onSortFilterChange = useCallback(
+    (e) => setSortFilter(e.target.value),
+    []
+  );
 
   return (
     <section className={styles["admin-users"]}>
       <h1>All Users</h1>
+      <div className={styles["admin-users-top"]}>
+        <FilterUsersBySort onChange={onSortFilterChange} />
+      </div>
       {isLoading ? (
         <Spinner />
       ) : (
@@ -39,7 +63,22 @@ const Users = () => {
           </AnimatePresence>
         </motion.div>
       )}
-      {deleteOpen && <h2>Open</h2>}
+      {deleteOpen && (
+        <DeleteUser
+          onClose={onCloseDelete}
+          updateUsers={updateUsers}
+          id={selectedUser}
+        />
+      )}
+      {pageCount > 1 && (
+        <Pagination
+          page={page}
+          pageCount={pageCount}
+          prevPage={prevPage}
+          nextPage={nextPage}
+          switchToPage={switchToPage}
+        />
+      )}
     </section>
   );
 };

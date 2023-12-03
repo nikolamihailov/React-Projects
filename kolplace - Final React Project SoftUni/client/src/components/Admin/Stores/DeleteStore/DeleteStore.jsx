ceb38328f76f3d2e293bd7a7e5f5b34e882c060c
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-03 02:35:19 +0200

    delete store added

---
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Stores/DeleteStore/DeleteStore.jsx
@@ -0,0 +1,58 @@
+import { useContext, useEffect, useState } from "react";
+import styles from "./DeleteStore.module.css";
+import { useNavigate } from "react-router-dom";
+import { NotifContext } from "../../../../contexts/NotificationContext";
+import { AuthContext } from "../../../../contexts/AuthContext";
+import { deleteStore, getOne } from "../../../../data/services/storeService";
+
+const DeleteStore = ({ onClose, id, updateStores }) => {
+  const [store, setStore] = useState(null);
+  const navigateTo = useNavigate();
+  const { updateNotifs } = useContext(NotifContext);
+  const { updateAuth } = useContext(AuthContext);
+
+  useEffect(() => {
+    getOne(id).then((data) => setStore(data));
+  }, [id]);
+
+  const onDelete = async () => {
+    const deletedStore = await deleteStore(id);
+    if (deletedStore.expMessage) {
+      updateNotifs([{ text: deletedStore.expMessage, type: "error" }]);
+      navigateTo("/login");
+      updateAuth({});
+    }
+
+    if (deletedStore.error) {
+      updateNotifs([{ text: deletedStore.error, type: "error" }]);
+    } else {
+      updateStores((stores) =>
+        stores.filter((s) => s._id !== deletedStore._id)
+      );
+      updateNotifs([
+        {
+          text: `Store - KolPlace ${deletedStore.city} deleted!`,
+          type: "success",
+        },
+      ]);
+      onClose();
+      navigateTo("/admin-panel/stores");
+    }
+  };
+  return (
+    <>
+      <div className="backdrop" onClick={onClose}></div>
+      <div className={styles["delete-store"]}>
+        <p>
+          Are you sure you want to delete <span>{store?.city} </span> store?
+        </p>
+        <div className={styles["btns"]}>
+          <button onClick={onDelete}>Ok</button>
+          <button onClick={onClose}>Cancel</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DeleteStore;

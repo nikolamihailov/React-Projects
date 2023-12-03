2461bb9790217387ce6d58fe51f659893fd57495
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-03 23:38:25 +0200

    refactored

--- kolplace - Final React Project SoftUni/server/src/middlewares/isAdmin.js
+++ kolplace - Final React Project SoftUni/server/src/middlewares/isAdmin.js
@@ -1,7 +1,12 @@
 exports.isAdmin = (req, res, next) => {
-    if (req.user.data.user && req.user?.data.user.role === "admin") {
+    if (req.user?.data.user && req.user?.data.user.role === "admin") {
         next();
     } else {
+        console.log("in");
+        if (req.decToken) {
+            console.log("in expired");
+            return res.status(401).json({ expMessage: "Your session has expired, you have to login again!" });
+        }
         res.status(401).json({ unathorized: "You have no power here!" });
     }
 };
--- kolplace - Final React Project SoftUni/server/src/middlewares/isAuthenticated.js
+++ kolplace - Final React Project SoftUni/server/src/middlewares/isAuthenticated.js
@@ -2,6 +2,9 @@ exports.isAuthenticated = (req, res, next) => {
     if (req.user?.data.user._id) {
         next();
     } else {
+        if (req.decToken) {
+            return res.status(401).json({ expMessage: "Your session has expired, you have to login again!" });
+        }
         res.status(401).json({ unathorized: "You have no power here!" });
     }
 };

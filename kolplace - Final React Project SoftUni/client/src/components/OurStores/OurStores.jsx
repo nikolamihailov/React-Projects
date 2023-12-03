455b9d9ca7f2052ada8f39e5e625e230a19ea15a
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-03 23:35:47 +0200

    our stores page updated

--- kolplace - Final React Project SoftUni/client/src/components/OurStores/OurStores.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/OurStores/OurStores.jsx
@@ -1,11 +1,52 @@
+import { useCallback, useEffect, useState } from "react";
 import useTitle from "../../hooks/useTitle";
+import { getAll } from "../../data/services/storeService";
+import Store from "./StoreItem/Store";
+import styles from "./OurStores.module.css";
+import { motion, AnimatePresence } from "framer-motion";
+import StoreMap from "./StoreMap/StoreMap";
 
 const OurStores = () => {
   useTitle("Our Stores | KolPlace");
+  const [stores, setStores] = useState([]);
+  const [selectedStore, setSelectedStore] = useState(null);
+  const [isBigMapOpen, setIsBigMapOpen] = useState(false);
+
+  useEffect(() => {
+    getAll().then((data) => {
+      setStores(data);
+    });
+  }, []);
+
+  const onClose = useCallback(() => setIsBigMapOpen(false), []);
+  const openBigMap = useCallback((id) => {
+    setSelectedStore(id);
+    setIsBigMapOpen(true);
+  }, []);
 
   return (
-    <section>
+    <section className={styles["our-stores-section"]}>
       <h1>Our Stores</h1>
+      <div className={styles["info"]}>
+        <p>
+          Since 2020, our stores have been curating an extensive collection to
+          bring you the latest trends in gadgets, fashion, books, and beyond.
+          Immerse yourself in the joy of discovery as we strive to cater to your
+          diverse tastes. Whether you&apos;re a tech enthusiast, a fashionista,
+          or a bookworm, KolPlace stores are designed to elevate your shopping
+          experience. Join us and indulge in a one-stop-shop where variety meets
+          convenience, and your satisfaction is our top priority.
+        </p>
+      </div>
+      <motion.div className={styles["stores"]}>
+        <AnimatePresence>
+          {stores.length > 0 &&
+            stores.map((s) => {
+              return <Store key={s._id} {...s} openBigMap={openBigMap} />;
+            })}
+        </AnimatePresence>
+      </motion.div>
+      {isBigMapOpen && <StoreMap id={selectedStore} onClose={onClose} />}
     </section>
   );
 };

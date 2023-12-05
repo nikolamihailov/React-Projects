7a0b0fcce0b8223772367d1d4bec94d39f6c3e43
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-05 23:53:54 +0200

    review item added

---
+++ kolplace - Final React Project SoftUni/client/src/components/ProductDetails/Reviews/ReviewsContainer/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,28 @@
+import { formatDate } from "../../../../../utils/dateFormatter";
+import styles from "./ReviewItem.module.css";
+
+const ReviewItem = ({ text, rating, author, createdAt }) => {
+  const stars = Array.from({ length: rating }, (_, index) => (
+    <i key={index} className="fa-solid fa-star"></i>
+  ));
+
+  return (
+    <div className={styles["review-item"]}>
+      <div className={styles["author"]}>
+        <img src={author.avatar} alt={author.firstName} />
+        <span>
+          {author.firstName} {author.lastName}
+        </span>
+      </div>
+      <div className={styles["content"]}>
+        <span>{stars}</span>
+        <p>{text}</p>
+      </div>
+      <div className={styles["created"]}>
+        <p>{formatDate(createdAt)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewItem;

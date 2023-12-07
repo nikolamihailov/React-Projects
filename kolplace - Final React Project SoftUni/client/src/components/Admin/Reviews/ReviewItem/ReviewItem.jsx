fcb11861ce97f2c9e89e18581d98e9f1731af0dd
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-07 02:29:35 +0200

    review item bumped

---
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Reviews/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,54 @@
+import { formatDate } from "../../../../utils/dateFormatter";
+import styles from "./ReviewItem.module.css";
+import { motion } from "framer-motion";
+
+const ReviewItem = ({
+  _id,
+  text,
+  rating,
+  author,
+  createdAt,
+  product,
+  openDelete,
+}) => {
+  const stars = Array.from({ length: rating }, (_, index) => (
+    <i key={index} className="fa-solid fa-star"></i>
+  ));
+
+  for (let i = 0; i < 5 - rating; i++) {
+    stars.push(<i key={i + rating} className="fa-regular fa-star"></i>);
+  }
+
+  return (
+    <motion.div
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      whileHover={{ scale: 1.01, y: -5, transition: 0.2 }}
+      className={styles["review-item"]}
+    >
+      <div className={styles["author"]}>
+        <img src={author.avatar} alt="avatar" />
+        <span>{author.firstName}</span>
+      </div>
+      <div className={styles["content"]}>
+        <span>{stars}</span>
+        <p>{text}</p>
+      </div>
+      <div className={styles["product"]}>
+        <img src={product.mainImage} alt="product" />
+      </div>
+      <div className={styles["createdAt"]}>
+        <span>{formatDate(createdAt)}</span>
+      </div>
+      <div className={styles["createdAt"]}>
+        <button onClick={() => openDelete(_id)} title={"Delete Review"}>
+          <i className="fa-solid fa-trash-can"></i>
+        </button>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ReviewItem;

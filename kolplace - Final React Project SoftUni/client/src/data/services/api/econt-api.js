85a8837ef7573923195174ff6407cd26869da2e6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-08 01:20:57 +0200

    econt API service added

---
+++ kolplace - Final React Project SoftUni/client/src/data/services/api/econt-api.js
@@ -0,0 +1,31 @@
+const ECONT_URL = 'http://ee.econt.com/services/Nomenclatures/NomenclaturesService';
+
+const econtEndpoints = {
+    getCities: ".getCities.json",
+    getOffices: ".getOffices.json",
+};
+
+export const getAllCities = async () => {
+    const cities = await fetch(ECONT_URL + econtEndpoints.getCities, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({ countryCode: "BGR" })
+    });
+
+    return await cities.json();
+};
+
+export const getAllOfficesInCity = async (id) => {
+    const cities = await fetch(ECONT_URL + econtEndpoints.getOffices, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({ countryCode: "BGR", cityID: id })
+    });
+
+    return await cities.json();
+};
+

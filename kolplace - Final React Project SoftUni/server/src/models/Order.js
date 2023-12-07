0266fc062be50c3b8069295ee4733c78812154be
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-08 01:21:44 +0200

    order model added

---
+++ kolplace - Final React Project SoftUni/server/src/models/Order.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required!"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required!"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required!"]
+    },
+    deliveryType: {
+        type: String,
+        required: [true, "Delivery is required!"]
+    },
+    address: {
+        type: String,
+        required: [true, "Address is required!"]
+    },
+    paymentMethod: {
+        type: String,
+        required: [true, "Payment method is required!"]
+    },
+    products: [{
+        product: {
+            type: mongoose.Types.ObjectId,
+            ref: "Product"
+        },
+        quantity: {
+            type: Number
+        }
+    }],
+    totalPrice: {
+        type: Number,
+        required: [true, "Total price is required!"]
+    },
+    maker: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    }
+
+}, { timestamps: true });
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;

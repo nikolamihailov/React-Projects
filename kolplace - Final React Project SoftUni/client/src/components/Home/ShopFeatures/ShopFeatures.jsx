11db47ec4c734a214c00d1bc4ec1a074559b429a
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-08 18:03:04 +0200

    updated shop feautures with links

--- kolplace - Final React Project SoftUni/client/src/components/Home/ShopFeatures/ShopFeatures.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Home/ShopFeatures/ShopFeatures.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./ShopFeatures.module.css";
 
 const ShopFeaturesSection = () => {
@@ -5,12 +6,24 @@ const ShopFeaturesSection = () => {
     <section className={styles["shopFeatures"]}>
       <div className={styles["feature"]}>
         <i className="fas fa-money-bill" />
-        <p>Money Back Guarantee</p>
+        <a
+          href="https://en.wikipedia.org/wiki/Money-back_guarantee"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>Money Back Guarantee</p>
+        </a>
       </div>
       <div className={styles["feature-slash"]}></div>
       <div className={styles["feature"]}>
         <i className="fas fa-shield-alt" />
-        <p>Price Defense Policy</p>
+        <a
+          href="  https://www.acq.osd.mil/asda/dpc/index.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>Price Defense Policy</p>
+        </a>
       </div>
       <div className={styles["feature-slash"]}></div>
       <div className={styles["feature"]}>
@@ -25,7 +38,10 @@ const ShopFeaturesSection = () => {
       <div className={styles["feature-slash"]}></div>
       <div className={styles["feature"]}>
         <i className="fas fa-store" />
-        <p>Our Stores</p>
+        <Link to={"/our-stores"}>
+          {" "}
+          <p>Our Stores</p>
+        </Link>
       </div>
     </section>
   );

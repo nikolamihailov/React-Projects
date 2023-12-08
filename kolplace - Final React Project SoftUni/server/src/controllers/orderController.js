ddd3debb7927fcb1e3d0f195d866ca29c318ea20
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-08 18:07:29 +0200

    order controller updated

--- kolplace - Final React Project SoftUni/server/src/controllers/orderController.js
+++ kolplace - Final React Project SoftUni/server/src/controllers/orderController.js
@@ -1,6 +1,7 @@
 const orderController = require("express").Router();
 const orderService = require("../services/orderService");
 const { isAuthenticated } = require("../middlewares/isAuthenticated");
+const { trimBody } = require("../middlewares/trimBody");
 const { extractErrors } = require("../utils/errParse");
 
 
@@ -8,6 +9,27 @@ orderController.post("/orders", isAuthenticated, async (req, res) => {
     try {
         const order = await orderService.createOrder(req.body);
         res.status(201).json(order);
+    } catch (error) {
+        const errors = extractErrors(error);
+        res.status(400).json({ errors });
+    }
+});
+
+
+orderController.get("/orders/:id", isAuthenticated, trimBody, async (req, res) => {
+    try {
+        const orders = await orderService.getAllOrdersForUser(req.params.id);
+        res.status(200).json(orders);
+    } catch (error) {
+        const errors = extractErrors(error);
+        res.status(400).json({ errors });
+    }
+});
+
+orderController.get("/orders/details/:id", isAuthenticated, trimBody, async (req, res) => {
+    try {
+        const order = await orderService.getOneOrder(req.params.id);
+        res.status(200).json(order);
     } catch (error) {
         const errors = extractErrors(error);
         res.status(400).json({ errors });

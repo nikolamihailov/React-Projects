bdba53abdac6c472e6e58a4c10b8331ecda560cc
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-08 18:06:22 +0200

    added get all and get one order

--- kolplace - Final React Project SoftUni/client/src/data/services/orderService.js
+++ kolplace - Final React Project SoftUni/client/src/data/services/orderService.js
@@ -2,7 +2,17 @@ const ORDERS_URL = "/orders";
 import { request } from "../requester";
 
 const orderEnpoints = {
+    getAll: ORDERS_URL + "/",
+    getOne: ORDERS_URL + "/details/",
     createOrder: ORDERS_URL + "",
+};
+
+export const getOneOrder = async (id) => {
+    return await request.get(orderEnpoints.getOne + id);
+};
+
+export const getAllMineOrders = async (id) => {
+    return await request.get(orderEnpoints.getAll + id);
 };
 
 export const createOrder = async (data) => {
--- kolplace - Final React Project SoftUni/server/src/services/orderService.js
+++ kolplace - Final React Project SoftUni/server/src/services/orderService.js
@@ -1,3 +1,7 @@
 const Order = require("../models/Order");
 
 exports.createOrder = (data) => Order.create(data);
+
+exports.getAllOrdersForUser = (id) => Order.find({ maker: id }).populate("products.product");
+
+exports.getOneOrder = (id) => Order.findById(id).populate("products.product");

730040cf8e3b8c8f82cf96d17f9f30c428e17d77
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-09 02:00:27 +0200

    config done

--- kolplace - Final React Project SoftUni/client/vite.config.js
+++ kolplace - Final React Project SoftUni/client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: './setup.js',
+  },
+});

5af25475e7bb97b5dcb3e9392776d1430b97acca
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-19 22:39:39 +0200

    demo project for typescript and tailwind started

---
+++ demo app/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div className="flex center gap-3 w-screen h-screen items-center justify-evenly">
+        <section>
+          <a href="">Link One</a>
+        </section>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Click me
+        </button>
+        <section>
+          <a href="">Link Two</a>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default App;

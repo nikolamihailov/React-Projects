b921f7775d4bd2342fae6d29e5f6cd3f2129fbd1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2023-12-20 22:57:01 +0200

    bumped small changes

--- demo app/src/App.tsx
+++ demo app/src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+
 function App() {
+  const [likes, setLikes] = useState<number>(0);
   return (
     <>
       <div className="flex center gap-3 w-screen h-screen items-center justify-evenly">
         <section>
           <a href="">Link One</a>
         </section>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Click me
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setLikes(likes + 1)}
+        >
+          Likes <span>{likes}</span>
         </button>
         <section>
           <a href="">Link Two</a>

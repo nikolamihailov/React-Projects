d410321be3e8c4a3297aaa38aee6613b71e0157a
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-01-02 23:18:11 +0200

    created container

--- demo app/src/App.tsx
+++ demo app/src/App.tsx
@@ -1,23 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [likes, setLikes] = useState<number>(0);
   return (
     <>
-      <div className="flex center gap-3 w-screen h-screen items-center justify-evenly">
-        <section>
-          <a href="">Link One</a>
-        </section>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setLikes(likes + 1)}
-        >
-          Likes <span>{likes}</span>
-        </button>
-        <section>
-          <a href="">Link Two</a>
-        </section>
-      </div>
+      <div className="flex center gap-3 w-screen h-screen items-center justify-evenly bg-gradient-to-br from-red-500 to-black"></div>
     </>
   );
 }

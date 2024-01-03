ca9e38195edd65ad3cad3a12258369fc4e72679a
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-01-03 23:23:25 +0200

    container changes added

--- demo app/src/App.tsx
+++ demo app/src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <div className="flex center gap-3 w-screen h-screen items-center justify-evenly bg-gradient-to-br from-red-500 to-black"></div>
+      <main className="flex w-screen h-screen items-center justify-evenly bg-gradient-to-br from-red-500 to-black">
+        <div className="flex flex-col items-center justify-center bg-white backdrop-blur-lg bg-opacity-15 w-4/5 h-3/5 rounded p-8">
+          <h1 className=" text-white font-bold text-2xl">Weather</h1>
+        </div>
+      </main>
     </>
   );
 }

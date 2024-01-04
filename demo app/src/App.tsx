4be77589c9aa8ee5ee8f03a6e6d89ca89d510155
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-01-04 23:18:28 +0200

    changed container bg gradient

--- demo app/src/App.tsx
+++ demo app/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <>
-      <main className="flex w-screen h-screen items-center justify-evenly bg-gradient-to-br from-red-500 to-black">
-        <div className="flex flex-col items-center justify-center bg-white backdrop-blur-lg bg-opacity-15 w-4/5 h-3/5 rounded p-8">
+      <main className="flex w-screen h-screen items-center justify-evenly bg-gradient-to-br from-blue-600 to-blue-900">
+        <div className="flex flex-col items-center justify-center bg-white backdrop-blur-xlg drop-shadow-xlg bg-opacity-15 w-4/5 h-3/5 rounded p-8">
           <h1 className=" text-white font-bold text-2xl">Weather</h1>
         </div>
       </main>

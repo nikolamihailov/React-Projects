4387eaa66f778d83b8fb1074a5121701c346b968
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-01-08 20:12:52 +0200

    returned previous

--- kolplace - Final React Project SoftUni/server/src/middlewares/cors.js
+++ kolplace - Final React Project SoftUni/server/src/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://kolplace.onrender.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods',
         'OPTIONS, GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers',

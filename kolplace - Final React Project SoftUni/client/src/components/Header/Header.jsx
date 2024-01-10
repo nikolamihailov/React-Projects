401439eb4049ef752afe3520602e7a2fbfa97a3c
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-01-10 23:06:03 +0200

    mobile nav added to header

--- kolplace - Final React Project SoftUni/client/src/components/Header/Header.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Header/Header.jsx
@@ -3,13 +3,24 @@ import MainNav from "../Navigation/NavigationMain/MainNav";
 import TopNav from "../Navigation/NavigationTop/TopNav";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import TopNavMobile from "../Navigation/NavigationMobile/NavigationTopMobile";
 
 const Header = () => {
   const [pageURL, setPageURL] = useState("/");
+  const [isMobile, setIsMobile] = useState(false);
   const loc = useLocation();
 
   useEffect(() => {
     setPageURL(loc.pathname);
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 900);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, [loc]);
   return (
     <>
@@ -17,8 +28,14 @@ const Header = () => {
         ""
       ) : (
         <header>
-          <TopNav />
-          <MainNav />
+          {isMobile ? (
+            <TopNavMobile />
+          ) : (
+            <>
+              <TopNav />
+              <MainNav />
+            </>
+          )}
         </header>
       )}
     </>

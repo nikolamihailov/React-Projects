b396d1b2af46c62e5f9c7f4fc98590750b4d5394
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-01-21 17:36:15 +0200

    fixed wrong usage of server endpoint

--- kolplace - Final React Project SoftUni/client/src/data/requester.js
+++ kolplace - Final React Project SoftUni/client/src/data/requester.js
@@ -1,5 +1,5 @@
-// const BASE_URL = import.meta.env.VITE_REST_API_BASE_URL; deployed server endpoint
-const BASE_URL = "http://localhost:3030";  // local test server endpoint
+const BASE_URL = import.meta.env.VITE_REST_API_BASE_URL; // deployed server endpoint
+// const BASE_URL = "http://localhost:3030";  // local test server endpoint
 
 
 const requester = async (method, URL, data) => {

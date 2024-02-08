7d117275c0ae9414c51f70a02f081f007ef6f055
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-02-08 23:07:04 +0200

    updated all right reserved year with function

--- solar-system-planets/src/components/Footer/Footer.jsx
+++ solar-system-planets/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>All rights reserved! &copy; 2023</p>
+      <p>All rights reserved! &copy; {new Date().getFullYear()}</p>
       <div className={styles["social-networks"]}>
         <ul>
           <li>
@@ -15,18 +15,12 @@ const Footer = () => {
             </a>
           </li>
           <li>
-            <a
-              href="https://github.com/nikolamihailov"
-              target="_blank"
-            >
+            <a href="https://github.com/nikolamihailov" target="_blank">
               <i className="fa-brands fa-github"></i>
             </a>
           </li>
           <li>
-            <a
-              href="https://www.instagram.com/nikolanm06/"
-              target="_blank"
-            >
+            <a href="https://www.instagram.com/nikolanm06/" target="_blank">
               <i className="fa-brands fa-instagram"></i>
             </a>
           </li>

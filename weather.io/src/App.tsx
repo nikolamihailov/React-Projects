d8ab00f78a0253c48cbec3c7b03349be4f092498
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-10 12:45:57 +0200

    formatted classes

--- weather.io/src/App.tsx
+++ weather.io/src/App.tsx
@@ -10,29 +10,32 @@ function App(): JSX.Element {
   };
   return (
     <>
-      <main className="flex w-screen h-screen items-center justify-evenly bg-gradient-to-br from-blue-700 to-blue-900">
-        <section className="md:max-w-3xl flex flex-col items-center text-center justify-center bg-white backdrop-blur-xlg drop-shadow-xlg bg-opacity-15 w-4/5 h-3/5 rounded p-8">
-          <h1 className="font-thin text-4xl md:text-5xl xl:text-6xl">
-            <span className="font-black bg-gradient-to-br  from-white via-orange-600 to-red-700 text-transparent bg-clip-text">
+      <main className="flex h-screen w-screen items-center justify-evenly bg-gradient-to-br from-blue-700 to-blue-900">
+        <section className="backdrop-blur-xlg drop-shadow-xlg flex h-3/5 w-4/5 flex-col items-center justify-center rounded bg-white bg-opacity-15 p-8 text-center md:max-w-3xl">
+          <h1 className="text-4xl font-thin md:text-5xl xl:text-6xl">
+            <span className="bg-gradient-to-br from-white  via-orange-600 to-red-700 bg-clip-text font-black text-transparent">
               Weather
             </span>
-            <span className="font-black bg-gradient-to-br from-blue-500 via-blue-400 to-white text-transparent bg-clip-text">
+            <span className="bg-gradient-to-br from-blue-500 via-blue-400 to-white bg-clip-text font-black text-transparent">
               Forecast
             </span>
           </h1>
-          <p className=" text-xl text-white mt-3">
+          <p className=" my-8 text-xl text-white">
             Welcome to your friendly weather teller. Enter your city name for
             more information.
           </p>
-          <div className="flex items-center rounded-2xl border-4 border-white px-4 py-3 mt-4">
+          <div className="relative flex items-center text-white">
             <input
-              className=" bg-transparent text-white text-xl mr-3"
+              className="rounded-full border  border-blue-400 bg-transparent px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-200 focus:outline-none focus:ring focus:ring-blue-400 md:px-6 md:py-3"
               type="text"
               placeholder="London..."
               value={city}
               onChange={onCityInputChange}
             />
-            <FontAwesomeIcon icon={faSearch} className="text-white" />
+            <FontAwesomeIcon
+              icon={faSearch}
+              className="absolute right-3 text-white"
+            />
           </div>
         </section>
       </main>

54cdc4eb270a8e2c76ae10f1c51f3819cea449c4
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-17 00:07:24 +0300

    added loading spinner to edit product in admin panel

--- kolplace - Final React Project SoftUni/client/src/components/Admin/Products/EditProductItem/EditProduct.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Products/EditProductItem/EditProduct.jsx
@@ -1,16 +1,14 @@
 import { useContext, useEffect, useState } from "react";
 import styles from "../AddProductItem/AddProduct.module.css";
 import { getAll } from "../../../../data/services/categoryService";
-import {
-  editProduct,
-  getOneProduct,
-} from "../../../../data/services/productService";
+import { editProduct, getOneProduct } from "../../../../data/services/productService";
 import { NotifContext } from "../../../../contexts/NotificationContext";
 import { AuthContext } from "../../../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import Notification from "../../../Notifications/Notification";
 import { v4 as uuidv4 } from "uuid";
 import noImage from "../../../../assets/no-image.jpg";
+import Spinner from "../../../Spinner/Spinner";
 
 const FORM_VALUES = {
   Name: "name",
@@ -39,6 +37,7 @@ const EditProductItem = ({ onClose, id, updateProducts }) => {
     [FORM_VALUES.ImageThree]: "",
     [FORM_VALUES.ImageFour]: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigateTo = useNavigate();
   const [errors, setErrors] = useState([]);
@@ -46,6 +45,7 @@ const EditProductItem = ({ onClose, id, updateProducts }) => {
   const { updateAuth } = useContext(AuthContext);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       try {
         const categories = await getAll();
@@ -66,6 +66,8 @@ const EditProductItem = ({ onClose, id, updateProducts }) => {
         });
       } catch (err) {
         console.error(err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -74,8 +76,7 @@ const EditProductItem = ({ onClose, id, updateProducts }) => {
 
   const onChange = (e) => {
     const { name, value, type, checked } = e.target;
-    if (type === "checkbox")
-      setValues((state) => ({ ...state, [name]: checked }));
+    if (type === "checkbox") setValues((state) => ({ ...state, [name]: checked }));
     else setValues((state) => ({ ...state, [name]: value }));
     setErrors([]);
   };
@@ -105,13 +106,9 @@ const EditProductItem = ({ onClose, id, updateProducts }) => {
       setErrors(Object.values(editedProduct.errors));
     } else {
       updateProducts((products) =>
-        products.map((product) =>
-          product._id === editedProduct._id ? editedProduct : product
-        )
+        products.map((product) => (product._id === editedProduct._id ? editedProduct : product))
       );
-      updateNotifs([
-        { text: `Product - ${editedProduct.name} updated!`, type: "success" },
-      ]);
+      updateNotifs([{ text: `Product - ${editedProduct.name} updated!`, type: "success" }]);
       setErrors([]);
       onClose();
       navigateTo("/admin-panel/products");
@@ -122,175 +119,159 @@ const EditProductItem = ({ onClose, id, updateProducts }) => {
     <>
       <div className="backdrop" onClick={onClose}></div>
       <form onSubmit={onSubmit} className={styles["add-product"]}>
-        <div className={styles["all-product-inputs"]}>
-          <div className={styles["inputs"]}>
-            <div className={styles["form-group"]}>
-              <label htmlFor={FORM_VALUES.Name}>Product Name</label>
-              <input
-                type="text"
-                name={FORM_VALUES.Name}
-                id={FORM_VALUES.Name}
-                placeholder="Iphone 15 Pro Max"
-                value={values[FORM_VALUES.Name] || ""}
-                onChange={onChange}
-              />
-            </div>
-            <div className={styles["form-group"]}>
-              <label htmlFor={FORM_VALUES.Price}>Product Price</label>
-              <input
-                type="number"
-                step={0.1}
-                name={FORM_VALUES.Price}
-                id={FORM_VALUES.Price}
-                placeholder="999"
-                value={values[FORM_VALUES.Price] || ""}
-                onChange={onChange}
-              />
-            </div>
-            <div className={styles["form-group"]}>
-              <label className={styles["container"]}>
-                Has Promo Price
-                <input
-                  type="checkbox"
-                  name={FORM_VALUES.HasPromoPrice}
-                  checked={values[FORM_VALUES.HasPromoPrice]}
-                  onChange={onChange}
-                />
-                <span className={styles["checkmark"]}></span>
-              </label>
-            </div>
-            {values[FORM_VALUES.HasPromoPrice] && (
-              <div className={styles["form-group"]}>
-                <label htmlFor={FORM_VALUES.PromoPrice}>
-                  Product Promo Price
-                </label>
-                <input
-                  type="number"
-                  step={0.1}
-                  name={FORM_VALUES.PromoPrice}
-                  id={FORM_VALUES.PromoPrice}
-                  placeholder="799"
-                  value={values[FORM_VALUES.PromoPrice] || ""}
-                  onChange={onChange}
-                />
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className={styles["all-product-inputs"]}>
+              <div className={styles["inputs"]}>
+                <div className={styles["form-group"]}>
+                  <label htmlFor={FORM_VALUES.Name}>Product Name</label>
+                  <input
+                    type="text"
+                    name={FORM_VALUES.Name}
+                    id={FORM_VALUES.Name}
+                    placeholder="Iphone 15 Pro Max"
+                    value={values[FORM_VALUES.Name] || ""}
+                    onChange={onChange}
+                  />
+                </div>
+                <div className={styles["form-group"]}>
+                  <label htmlFor={FORM_VALUES.Price}>Product Price</label>
+                  <input
+                    type="number"
+                    step={0.1}
+                    name={FORM_VALUES.Price}
+                    id={FORM_VALUES.Price}
+                    placeholder="999"
+                    value={values[FORM_VALUES.Price] || ""}
+                    onChange={onChange}
+                  />
+                </div>
+                <div className={styles["form-group"]}>
+                  <label className={styles["container"]}>
+                    Has Promo Price
+                    <input
+                      type="checkbox"
+                      name={FORM_VALUES.HasPromoPrice}
+                      checked={values[FORM_VALUES.HasPromoPrice]}
+                      onChange={onChange}
+                    />
+                    <span className={styles["checkmark"]}></span>
+                  </label>
+                </div>
+                {values[FORM_VALUES.HasPromoPrice] && (
+                  <div className={styles["form-group"]}>
+                    <label htmlFor={FORM_VALUES.PromoPrice}>Product Promo Price</label>
+                    <input
+                      type="number"
+                      step={0.1}
+                      name={FORM_VALUES.PromoPrice}
+                      id={FORM_VALUES.PromoPrice}
+                      placeholder="799"
+                      value={values[FORM_VALUES.PromoPrice] || ""}
+                      onChange={onChange}
+                    />
+                  </div>
+                )}
+                <div className={styles["form-group"]}>
+                  <label htmlFor={FORM_VALUES.Category}>Category</label>
+                  <select
+                    name={FORM_VALUES.Category}
+                    id={FORM_VALUES.Category}
+                    onChange={onChange}
+                    value={values[FORM_VALUES.Category]}
+                  >
+                    {categories?.map((c) => (
+                      <option key={c._id} value={c._id}>
+                        {c.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className={styles["form-group"]}>
+                  <label htmlFor={FORM_VALUES.Description}>Product Description</label>
+                  <textarea
+                    name={FORM_VALUES.Description}
+                    id={FORM_VALUES.Description}
+                    value={values[FORM_VALUES.Description] || ""}
+                    placeholder="The Ihone 15 Pro Max is the only ..."
+                    onChange={onChange}
+                  ></textarea>
+                </div>
               </div>
-            )}
-            <div className={styles["form-group"]}>
-              <label htmlFor={FORM_VALUES.Category}>Category</label>
-              <select
-                name={FORM_VALUES.Category}
-                id={FORM_VALUES.Category}
-                onChange={onChange}
-                value={values[FORM_VALUES.Category]}
-              >
-                {categories?.map((c) => (
-                  <option key={c._id} value={c._id}>
-                    {c.name}
-                  </option>
-                ))}
-              </select>
+              <div className={styles["images"]}>
+                <div className={styles["form-group"]}>
+                  <label htmlFor={FORM_VALUES.Image}>Product Image</label>
+                  <input
+                    type="text"
+                    name={FORM_VALUES.Image}
+                    placeholder="https://iphone-15..."
+                    id={FORM_VALUES.Image}
+                    value={values[FORM_VALUES.Image] || ""}
+                    onChange={onChange}
+                  />
+                  {values[FORM_VALUES.Image] === "" ? (
+                    <img src={noImage} alt={values[FORM_VALUES.Name]} />
+                  ) : (
+                    <img src={values[FORM_VALUES.Image]} alt={values[FORM_VALUES.Name]} />
+                  )}
+                </div>
+                <div className={styles["form-group"]}>
+                  <label htmlFor={FORM_VALUES.ImageTwo}>Product Image 2 (Optional)</label>
+                  <input
+                    type="text"
+                    name={FORM_VALUES.ImageTwo}
+                    placeholder="https://iphone-15..."
+                    id={FORM_VALUES.ImageTwo}
+                    value={values[FORM_VALUES.ImageTwo] || ""}
+                    onChange={onChange}
+                  />
+                  {values[FORM_VALUES.ImageTwo] === "" ? (
+                    <img src={noImage} alt={values[FORM_VALUES.Name]} />
+                  ) : (
+                    <img src={values[FORM_VALUES.ImageTwo]} alt={values[FORM_VALUES.Name]} />
+                  )}
+                </div>
+                <div className={styles["form-group"]}>
+                  <label htmlFor={FORM_VALUES.ImageThree}>Product Image 3 (Optional)</label>
+                  <input
+                    type="text"
+                    name={FORM_VALUES.ImageThree}
+                    placeholder="https://iphone-15..."
+                    id={FORM_VALUES.ImageThree}
+                    value={values[FORM_VALUES.ImageThree] || ""}
+                    onChange={onChange}
+                  />
+                  {values[FORM_VALUES.ImageThree] === "" ? (
+                    <img src={noImage} alt={values[FORM_VALUES.Name]} />
+                  ) : (
+                    <img src={values[FORM_VALUES.ImageThree]} alt={values[FORM_VALUES.Name]} />
+                  )}
+                </div>
+                <div className={styles["form-group"]}>
+                  <label htmlFor={FORM_VALUES.ImageFour}>Product Image 4 (Optional)</label>
+                  <input
+                    type="text"
+                    name={FORM_VALUES.ImageFour}
+                    placeholder="https://iphone-15..."
+                    id={FORM_VALUES.ImageFour}
+                    value={values[FORM_VALUES.ImageFour] || ""}
+                    onChange={onChange}
+                  />
+                  {values[FORM_VALUES.ImageFour] === "" ? (
+                    <img src={noImage} alt={values[FORM_VALUES.Name]} />
+                  ) : (
+                    <img src={values[FORM_VALUES.ImageFour]} alt={values[FORM_VALUES.Name]} />
+                  )}
+                </div>
+              </div>
             </div>
-            <div className={styles["form-group"]}>
-              <label htmlFor={FORM_VALUES.Description}>
-                Product Description
-              </label>
-              <textarea
-                name={FORM_VALUES.Description}
-                id={FORM_VALUES.Description}
-                value={values[FORM_VALUES.Description] || ""}
-                placeholder="The Ihone 15 Pro Max is the only ..."
-                onChange={onChange}
-              ></textarea>
-            </div>
-          </div>
-          <div className={styles["images"]}>
-            <div className={styles["form-group"]}>
-              <label htmlFor={FORM_VALUES.Image}>Product Image</label>
-              <input
-                type="text"
-                name={FORM_VALUES.Image}
-                placeholder="https://iphone-15..."
-                id={FORM_VALUES.Image}
-                value={values[FORM_VALUES.Image] || ""}
-                onChange={onChange}
-              />
-              {values[FORM_VALUES.Image] === "" ? (
-                <img src={noImage} alt={values[FORM_VALUES.Name]} />
-              ) : (
-                <img
-                  src={values[FORM_VALUES.Image]}
-                  alt={values[FORM_VALUES.Name]}
-                />
-              )}
-            </div>
-            <div className={styles["form-group"]}>
-              <label htmlFor={FORM_VALUES.ImageTwo}>
-                Product Image 2 (Optional)
-              </label>
-              <input
-                type="text"
-                name={FORM_VALUES.ImageTwo}
-                placeholder="https://iphone-15..."
-                id={FORM_VALUES.ImageTwo}
-                value={values[FORM_VALUES.ImageTwo] || ""}
-                onChange={onChange}
-              />
-              {values[FORM_VALUES.ImageTwo] === "" ? (
-                <img src={noImage} alt={values[FORM_VALUES.Name]} />
-              ) : (
-                <img
-                  src={values[FORM_VALUES.ImageTwo]}
-                  alt={values[FORM_VALUES.Name]}
-                />
-              )}
-            </div>
-            <div className={styles["form-group"]}>
-              <label htmlFor={FORM_VALUES.ImageThree}>
-                Product Image 3 (Optional)
-              </label>
-              <input
-                type="text"
-                name={FORM_VALUES.ImageThree}
-                placeholder="https://iphone-15..."
-                id={FORM_VALUES.ImageThree}
-                value={values[FORM_VALUES.ImageThree] || ""}
-                onChange={onChange}
-              />
-              {values[FORM_VALUES.ImageThree] === "" ? (
-                <img src={noImage} alt={values[FORM_VALUES.Name]} />
-              ) : (
-                <img
-                  src={values[FORM_VALUES.ImageThree]}
-                  alt={values[FORM_VALUES.Name]}
-                />
-              )}
-            </div>
-            <div className={styles["form-group"]}>
-              <label htmlFor={FORM_VALUES.ImageFour}>
-                Product Image 4 (Optional)
-              </label>
-              <input
-                type="text"
-                name={FORM_VALUES.ImageFour}
-                placeholder="https://iphone-15..."
-                id={FORM_VALUES.ImageFour}
-                value={values[FORM_VALUES.ImageFour] || ""}
-                onChange={onChange}
-              />
-              {values[FORM_VALUES.ImageFour] === "" ? (
-                <img src={noImage} alt={values[FORM_VALUES.Name]} />
-              ) : (
-                <img
-                  src={values[FORM_VALUES.ImageFour]}
-                  alt={values[FORM_VALUES.Name]}
-                />
-              )}
-            </div>
-          </div>
-        </div>
-
-        <button type="submit">Edit</button>
+            <button type="submit">Edit</button>
+          </>
+        )}
       </form>
+
       {errors.length > 0 && (
         <div className={styles["errors-container"]}>
           {errors.map((e) => (

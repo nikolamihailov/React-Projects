fa398ebc27c28b698f7ac79a146ef60a4cc06530
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-17 00:07:55 +0300

    added loading spinner to admin info in admin panel

--- kolplace - Final React Project SoftUni/client/src/components/Navigation/NavigationAdmin/AdminInfo/AdminInfo.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Navigation/NavigationAdmin/AdminInfo/AdminInfo.jsx
@@ -4,27 +4,38 @@ import { AuthContext } from "../../../../contexts/AuthContext";
 import { Link } from "react-router-dom";
 import { getProfile } from "../../../../data/services/userService";
 import avatar from "../../../../assets/avatar.png";
+import Spinner from "../../../Spinner/Spinner";
 
 const AdminInfo = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [profile, setProfile] = useState({});
   const { auth } = useContext(AuthContext);
 
   useEffect(() => {
+    setIsLoading(true);
     getProfile(auth.user?._id)
       .then((data) => {
         setProfile(data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
   }, [auth.user?._id]);
+
   return (
     <div className={styles["info"]}>
-      <Link to={"/admin-panel"}>
-        <img src={profile?.avatar ? profile.avatar : avatar} alt="avatar" />
-      </Link>
-      <p>
-        {profile?.firstName} {profile?.lastName}
-      </p>
-      <p>Role: {profile?.role}</p>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to={"/admin-panel"}>
+            <img src={profile?.avatar ? profile.avatar : avatar} alt="avatar" />
+          </Link>
+          <p>
+            {profile?.firstName} {profile?.lastName}
+          </p>
+          <p>Role: {profile?.role}</p>
+        </>
+      )}
     </div>
   );
 };

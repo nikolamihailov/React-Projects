818edd4deeefae47d26d7b7eb4c50e510b942e25
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-18 20:16:16 +0300

    added loading spinner before store deletion for confirmation screen

--- kolplace - Final React Project SoftUni/client/src/components/Admin/Stores/DeleteStore/DeleteStore.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Stores/DeleteStore/DeleteStore.jsx
@@ -4,15 +4,20 @@ import { useNavigate } from "react-router-dom";
 import { NotifContext } from "../../../../contexts/NotificationContext";
 import { AuthContext } from "../../../../contexts/AuthContext";
 import { deleteStore, getOne } from "../../../../data/services/storeService";
+import Spinner from "../../../Spinner/Spinner";
 
 const DeleteStore = ({ onClose, id, updateStores }) => {
   const [store, setStore] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const navigateTo = useNavigate();
   const { updateNotifs } = useContext(NotifContext);
   const { updateAuth } = useContext(AuthContext);
 
   useEffect(() => {
-    getOne(id).then((data) => setStore(data));
+    setIsLoading(true);
+    getOne(id)
+      .then((data) => setStore(data))
+      .finally(() => setIsLoading(false));
   }, [id]);
 
   const onDelete = async () => {
@@ -26,9 +31,7 @@ const DeleteStore = ({ onClose, id, updateStores }) => {
     if (deletedStore.error) {
       updateNotifs([{ text: deletedStore.error, type: "error" }]);
     } else {
-      updateStores((stores) =>
-        stores.filter((s) => s._id !== deletedStore._id)
-      );
+      updateStores((stores) => stores.filter((s) => s._id !== deletedStore._id));
       updateNotifs([
         {
           text: `Store - KolPlace ${deletedStore.city} deleted!`,
@@ -43,13 +46,19 @@ const DeleteStore = ({ onClose, id, updateStores }) => {
     <>
       <div className="backdrop" onClick={onClose}></div>
       <div className={styles["delete-store"]}>
-        <p>
-          Are you sure you want to delete <span>{store?.city} </span> store?
-        </p>
-        <div className={styles["btns"]}>
-          <button onClick={onDelete}>Ok</button>
-          <button onClick={onClose}>Cancel</button>
-        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <p>
+              Are you sure you want to delete <span>{store?.city} </span> store?
+            </p>
+            <div className={styles["btns"]}>
+              <button onClick={onDelete}>Ok</button>
+              <button onClick={onClose}>Cancel</button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );

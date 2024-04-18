24bbf5972acb4aeada8208b5a7f707429ff2b3c6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-18 20:14:39 +0300

    added loading spinner before review deletion for confirmation screen

--- kolplace - Final React Project SoftUni/client/src/components/Admin/Reviews/DeleteReview/DeleteReview.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Reviews/DeleteReview/DeleteReview.jsx
@@ -4,17 +4,22 @@ import { useNavigate } from "react-router-dom";
 import { NotifContext } from "../../../../contexts/NotificationContext";
 import { AuthContext } from "../../../../contexts/AuthContext";
 import { deleteReview, getOne } from "../../../../data/services/reviewService";
+import Spinner from "../../../Spinner/Spinner";
 
 const DeleteReview = ({ onClose, id, updateReviews }) => {
   const [review, setReview] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const navigateTo = useNavigate();
   const { updateNotifs } = useContext(NotifContext);
   const { updateAuth } = useContext(AuthContext);
 
   useEffect(() => {
-    getOne(id).then((data) => {
-      setReview(data);
-    });
+    setIsLoading(true);
+    getOne(id)
+      .then((data) => {
+        setReview(data);
+      })
+      .finally(() => setIsLoading(false));
   }, [id]);
 
   const onDelete = async () => {
@@ -28,9 +33,7 @@ const DeleteReview = ({ onClose, id, updateReviews }) => {
     if (deletedReview.error) {
       updateNotifs([{ text: deletedReview.error, type: "error" }]);
     } else {
-      updateReviews((reviews) =>
-        reviews.filter((r) => r._id !== deletedReview._id)
-      );
+      updateReviews((reviews) => reviews.filter((r) => r._id !== deletedReview._id));
       updateNotifs([
         {
           text: `Review by ${deletedReview.author.firstName} on ${deletedReview.product.name} deleted!`,
@@ -45,15 +48,20 @@ const DeleteReview = ({ onClose, id, updateReviews }) => {
     <>
       <div className="backdrop" onClick={onClose}></div>
       <div className={styles["delete-review"]}>
-        <p>
-          Are you sure you want to delete review by
-          <span> {review?.author.firstName}</span> on{" "}
-          <span>{review?.product.name}</span>?
-        </p>
-        <div className={styles["btns"]}>
-          <button onClick={onDelete}>Ok</button>
-          <button onClick={onClose}>Cancel</button>
-        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <p>
+              Are you sure you want to delete review by
+              <span> {review?.author.firstName}</span> on <span>{review?.product.name}</span>?
+            </p>
+            <div className={styles["btns"]}>
+              <button onClick={onDelete}>Ok</button>
+              <button onClick={onClose}>Cancel</button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );

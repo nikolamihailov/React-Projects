87b1404a01084e50797ad2757958d2f079f390b8
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-04-18 20:12:59 +0300

    added loading spinner before product deletion for confirmation screen

--- kolplace - Final React Project SoftUni/client/src/components/Admin/Products/DeleteProductItem/DeleteProductItem.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/Admin/Products/DeleteProductItem/DeleteProductItem.jsx
@@ -3,19 +3,21 @@ import styles from "./DeleteProduct.module.css";
 import { NotifContext } from "../../../../contexts/NotificationContext";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../../contexts/AuthContext";
-import {
-  getOneProduct,
-  deleteProduct,
-} from "../../../../data/services/productService";
+import { getOneProduct, deleteProduct } from "../../../../data/services/productService";
+import Spinner from "../../../Spinner/Spinner";
 
 const DeleteProductItem = ({ onClose, id, updateProducts }) => {
   const [product, setProduct] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const navigateTo = useNavigate();
   const { updateNotifs } = useContext(NotifContext);
   const { updateAuth } = useContext(AuthContext);
 
   useEffect(() => {
-    getOneProduct(id).then((data) => setProduct(data));
+    setIsLoading(true);
+    getOneProduct(id)
+      .then((data) => setProduct(data))
+      .finally(() => setIsLoading(false));
   }, [id]);
 
   const onDelete = async () => {
@@ -28,9 +30,7 @@ const DeleteProductItem = ({ onClose, id, updateProducts }) => {
     if (deletedProduct.error) {
       updateNotifs([{ text: deletedProduct.error, type: "error" }]);
     } else {
-      updateProducts((products) =>
-        products.filter((p) => p._id !== deletedProduct._id)
-      );
+      updateProducts((products) => products.filter((p) => p._id !== deletedProduct._id));
       updateNotifs([
         {
           text: `Product - ${deletedProduct.name} deleted!`,
@@ -45,14 +45,19 @@ const DeleteProductItem = ({ onClose, id, updateProducts }) => {
     <>
       <div className="backdrop" onClick={onClose}></div>
       <div className={styles["delete-product"]}>
-        <p>
-          Are you sure you want to delete the product{" "}
-          <span>{product?.name}</span>?
-        </p>
-        <div>
-          <button onClick={onDelete}>Ok</button>
-          <button onClick={onClose}>Cancel</button>
-        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <p>
+              Are you sure you want to delete the product <span>{product?.name}</span>?
+            </p>
+            <div>
+              <button onClick={onDelete}>Ok</button>
+              <button onClick={onClose}>Cancel</button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );

7d261dc219d4fbf09f79c5d547599a6f52d75bae
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-06-23 23:04:30 +0300

    update

--- weather.io/src/App.tsx
+++ weather.io/src/App.tsx
@@ -2,14 +2,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { ChangeEvent, useState } from "react";
 
-const API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
-const BASE_URL = import.meta.env.VITE_OPEN_WEATHER_BASE_URL;
-const GEO_API = import.meta.env.VITE_OPEN_WEATHER_GEO_API;
-const LIMIT = import.meta.env.VITE_CITY_DROPDOWN_LIMIT;
-const FLAGS_API = import.meta.env.VITE_FLAGS_API;
-const FLAGS_STYLE = import.meta.env.VITE_FLAGS_STYLE;
-const FLAGS_SIZE = import.meta.env.VITE_FLAGS_SIZE;
-const WEATHER_API = import.meta.env.VITE_OPEN_WEATHER_WEATHER_API;
+const {
+  VITE_OPEN_WEATHER_API_KEY: API_KEY,
+  VITE_OPEN_WEATHER_BASE_URL: BASE_URL,
+  VITE_OPEN_WEATHER_GEO_API: GEO_API,
+  VITE_CITY_DROPDOWN_LIMIT: LIMIT,
+  VITE_FLAGS_API: FLAGS_API,
+  VITE_FLAGS_STYLE: FLAGS_STYLE,
+  VITE_FLAGS_SIZE: FLAGS_SIZE,
+  VITE_OPEN_WEATHER_WEATHER_API: WEATHER_API,
+} = import.meta.env;
 
 type City = {
   name: string;

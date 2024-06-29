fa9e47cce00e640f545668ce7c7f3420f7d7369d
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-06-29 15:56:36 +0300

    refactored our stores

--- kolplace - Final React Project SoftUni/client/src/components/OurStores/OurStores.jsx
+++ kolplace - Final React Project SoftUni/client/src/components/OurStores/OurStores.jsx
@@ -5,7 +5,8 @@ import Store from "./StoreItem/Store";
 import styles from "./OurStores.module.css";
 import { motion, AnimatePresence } from "framer-motion";
 import StoreMap from "./StoreMap/StoreMap";
-import Spinner from "../Spinner/Spinner";
+import StoreSkeleton from "./StoreItem/StoreSkeleton";
+// import Spinner from "../Spinner/Spinner";
 
 const OurStores = () => {
   useTitle("Our Stores | KolPlace");
@@ -29,34 +30,32 @@ const OurStores = () => {
 
   return (
     <section className={styles["our-stores-section"]}>
-      {isLoading ? (
+      {/*  {isLoading ? (
         <Spinner />
-      ) : (
-        <>
-          <h1>Our Stores</h1>
-          <div className={styles["info"]}>
-            <p>
-              Since 2020, our stores have been curating an extensive collection
-              to bring you the latest trends in gadgets, fashion, books, and
-              beyond. Immerse yourself in the joy of discovery as we strive to
-              cater to your diverse tastes. Whether you&apos;re a tech
-              enthusiast, a fashionista, or a bookworm, KolPlace stores are
-              designed to elevate your shopping experience. Join us and indulge
-              in a one-stop-shop where variety meets convenience, and your
-              satisfaction is our top priority.
-            </p>
-          </div>
-          <motion.div className={styles["stores"]}>
-            <AnimatePresence>
-              {stores.length > 0 &&
-                stores.map((s) => {
-                  return <Store key={s._id} {...s} openBigMap={openBigMap} />;
-                })}
-            </AnimatePresence>
-          </motion.div>
-          {isBigMapOpen && <StoreMap id={selectedStore} onClose={onClose} />}
-        </>
-      )}
+      ) : ( */}
+      <>
+        <h1>Our Stores</h1>
+        <div className={styles["info"]}>
+          <p>
+            Since 2020, our stores have been curating an extensive collection to bring you the
+            latest trends in gadgets, fashion, books, and beyond. Immerse yourself in the joy of
+            discovery as we strive to cater to your diverse tastes. Whether you&apos;re a tech
+            enthusiast, a fashionista, or a bookworm, KolPlace stores are designed to elevate your
+            shopping experience. Join us and indulge in a one-stop-shop where variety meets
+            convenience, and your satisfaction is our top priority.
+          </p>
+        </div>
+        <motion.div className={styles["stores"]}>
+          <AnimatePresence>
+            {isLoading && <StoreSkeleton stores={3} />}
+            {stores.length > 0 &&
+              stores.map((s) => {
+                return <Store key={s._id} {...s} openBigMap={openBigMap} />;
+              })}
+          </AnimatePresence>
+        </motion.div>
+        {isBigMapOpen && <StoreMap id={selectedStore} onClose={onClose} />}
+      </>
     </section>
   );
 };

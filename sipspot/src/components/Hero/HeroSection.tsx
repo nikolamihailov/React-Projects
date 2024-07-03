0146e4f08bce0fe48f4451fcb33aad788c5cd831
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-03 22:41:04 +0300

    used custom section and button

--- sipspot/src/components/Hero/HeroSection.tsx
+++ sipspot/src/components/Hero/HeroSection.tsx
@@ -1,22 +1,25 @@
 import heroImg from "../../../public/hero-img.png";
-import Button from "../UI/Button";
+import Button from "../UI/Button/Button";
+import Section from "../UI/Section/Section";
 import styles from "./Hero.module.css";
 
 function HeroSection() {
   return (
-    <section className={styles["hero-section"]}>
+    <Section>
       <div className={styles["text-box"]}>
         <h1 className={styles["hero-heading"]}>Discover the Ultimate Cocktail Experience</h1>
         <p className={styles["hero-desc"]}>
           Crafting the finest cocktails for your delight. Every sip, a new story. Cheers to
           unforgettable moments!
         </p>
-        <Button>Explore Our Cocktails</Button>
+        <Button bgColor="#f59f00" color="white">
+          Explore Our Cocktails
+        </Button>
       </div>
       <div className={styles["img-box"]}>
         <img src={heroImg} alt="Delicious cocktails" />
       </div>
-    </section>
+    </Section>
   );
 }
 

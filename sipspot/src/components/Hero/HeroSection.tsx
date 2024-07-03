9b5d9e2bc94f5505c4f7f9c1838344ddae58d126
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-03 22:52:16 +0300

    testing colors added

--- sipspot/src/components/Hero/HeroSection.tsx
+++ sipspot/src/components/Hero/HeroSection.tsx
@@ -5,14 +5,14 @@ import styles from "./Hero.module.css";
 
 function HeroSection() {
   return (
-    <Section>
+    <Section bgColor="#ffd43b">
       <div className={styles["text-box"]}>
         <h1 className={styles["hero-heading"]}>Discover the Ultimate Cocktail Experience</h1>
         <p className={styles["hero-desc"]}>
           Crafting the finest cocktails for your delight. Every sip, a new story. Cheers to
           unforgettable moments!
         </p>
-        <Button bgColor="#f59f00" color="#fff">
+        <Button bgColor="#f08c00" color="#000">
           Explore Our Cocktails
         </Button>
       </div>

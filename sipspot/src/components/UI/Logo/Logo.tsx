19679a7b5e0b094a5d0de8af70514d4c86f5e5d6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-04 23:11:58 +0300

    custom logo component added

---
+++ sipspot/src/components/UI/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import logo from "../../../../public/logo-icon.png";
+
+function Logo() {
+  return (
+    <div>
+      <img src={logo} alt="logo for sipspot bar" height={60} width={60} />
+    </div>
+  );
+}
+
+export default Logo;

48d82fee0a21fba66b4fc48145cb902b8f7e9a63
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-04 18:38:45 +0300

    progress bumped

--- sipspot/src/components/Hero/HeroSection.tsx
+++ sipspot/src/components/Hero/HeroSection.tsx
@@ -5,19 +5,24 @@ import styles from "./Hero.module.css";
 
 function HeroSection() {
   return (
-    <Section bgColor="#ffd43b">
-      <div className={styles["text-box"]}>
-        <h1 className={styles["hero-heading"]}>Discover the Ultimate Cocktail Experience</h1>
-        <p className={styles["hero-desc"]}>
-          Crafting the finest cocktails for your delight. Every sip, a new story. Cheers to
-          unforgettable moments!
-        </p>
-        <Button bgColor="#f08c00" color="#000">
-          Explore Our Cocktails
-        </Button>
-      </div>
-      <div className={styles["img-box"]}>
-        <img src={heroImg} alt="Delicious cocktails" />
+    <Section bgColor="#ffe066">
+      <div className={styles["hero"]}>
+        <div className={styles["text-box"]}>
+          <h1 className={styles["hero-heading"]}>Discover the Ultimate Cocktail Experience</h1>
+          <p className={styles["hero-desc"]}>
+            Crafting the finest cocktails for your delight. Every sip, a new story. Cheers to
+            unforgettable moments!
+          </p>
+          <Button bgColor="#8B0401" color="#fff">
+            Explore Cocktails
+          </Button>
+          <Button bgColor="#f08c00" color="#fff">
+            Book Reservation
+          </Button>
+        </div>
+        <div className={styles["img-box"]}>
+          <img src={heroImg} alt="Delicious cocktails" />
+        </div>
       </div>
     </Section>
   );

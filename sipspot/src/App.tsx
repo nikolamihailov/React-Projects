4c9ff7cdf0250049ab7bb432d7bdcd2d17e949c0
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-04 23:32:45 +0300

    added react router to app.tsx

--- sipspot/src/App.tsx
+++ sipspot/src/App.tsx
@@ -1,10 +1,35 @@
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Gallery from "./pages/Gallery";
+import About from "./pages/About";
+
+import "./index.css";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/recipes",
+      element: <Recipes />,
+    },
+    {
+      path: "/gallery",
+      element: <Gallery />,
+    },
+    {
+      path: "/about-us",
+      element: <About />,
+    },
+  ]);
+
   return (
     <main>
-      <Home />
+      <RouterProvider router={router} />
     </main>
   );
 }

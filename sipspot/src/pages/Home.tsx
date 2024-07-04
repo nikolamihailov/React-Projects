8a1f2b3616e863b557281406fe4128722588d752
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-04 23:12:22 +0300

    navigation added to home page

--- sipspot/src/pages/Home.tsx
+++ sipspot/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import HeroSection from "../components/Hero/HeroSection";
+import Navigation from "../components/Navigation/Navigation";
 
 function Home() {
   return (
     <>
+      <Navigation />
       <HeroSection />
     </>
   );

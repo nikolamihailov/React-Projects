1702eb7c750ab15fe63a6e855fb6e400502c7b0e
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-05 19:42:36 +0300

    extracted navigation from home page to common layout page

--- sipspot/src/pages/Home.tsx
+++ sipspot/src/pages/Home.tsx
@@ -1,10 +1,8 @@
 import HeroSection from "../components/Hero/HeroSection";
-import Navigation from "../components/Navigation/Navigation";
 
 function Home() {
   return (
     <>
-      <Navigation />
       <HeroSection />
     </>
   );

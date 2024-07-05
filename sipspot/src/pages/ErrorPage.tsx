631f3f3b3cbd8e83350d18c8a317f08ac8ca0df6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-05 19:41:50 +0300

    basic error page created

---
+++ sipspot/src/pages/ErrorPage.tsx
@@ -0,0 +1,11 @@
+import Section from "../components/UI/Section/Section";
+
+function ErrorPage() {
+  return (
+    <Section bgColor="#fff">
+      <h1>Page not found!</h1>
+    </Section>
+  );
+}
+
+export default ErrorPage;

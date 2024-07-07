b2b61c288f324902ea4af6378e08f382575048dc
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-07-07 14:20:42 +0300

    using the router from import

--- sipspot/src/App.tsx
+++ sipspot/src/App.tsx
@@ -1,47 +1,12 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Recipes from "./pages/Recipes";
-import Gallery from "./pages/Gallery";
-import About from "./pages/About";
-import ErrorPage from "./pages/ErrorPage";
+import { RouterProvider } from "react-router-dom";
+import { appRouter } from "./routes/routes";
 
 import "./index.css";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      element: <Layout />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/recipes",
-          element: <Recipes />,
-        },
-        {
-          path: "/gallery",
-          element: <Gallery />,
-        },
-        {
-          path: "/about-us",
-          element: <About />,
-        },
-        {
-          path: "/*",
-          element: <ErrorPage />,
-        },
-      ],
-    },
-  ]);
-
   return (
     <main>
-      <RouterProvider router={router} />
+      <RouterProvider router={appRouter} />
     </main>
   );
 }

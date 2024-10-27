9453dcc28442a82eb5535beaaf7f711c015613be
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-10-27 21:24:31 +0200

    error page fixed

--- Flow Project/FE/src/pages/ErrorPage.tsx
+++ Flow Project/FE/src/pages/ErrorPage.tsx
@@ -1,9 +1,10 @@
-import { useTheme } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import Section from "../components/UI/Section/Section";
 import errorImg from "/sad.png";
 import useTitle from "../hooks/useTitle";
-import { errorPageStyles } from "../utils/StylesHelper/ErrorPage";
+import { errorImgSadFace, errorP, errorPageStyles } from "../utils/StylesHelper/ErrorPage";
 import Button from "../components/UI/Button/Button";
+import { Box } from "@mui/system";
 
 function ErrorPage() {
   const theme = useTheme();
@@ -13,7 +14,11 @@ function ErrorPage() {
   return (
     <Section bgColor={theme.palette.secondary.dark} style={errorPageStyles(theme)}>
       <h1 className="heading-primary">Looks Like We're Lost!</h1>
-      <img src={errorImg} alt="sad face" style={{ width: "30rem" }} />
+      <Typography variant="body1" sx={errorP(theme)}>
+        Sometimes even the best journey needs a little redirection. Let's find our way back to
+        wellness!
+      </Typography>
+      <Box component="img" src={errorImg} alt="sad face" sx={errorImgSadFace} />
       <Button variant="secondary" el="link" href="/home">
         Go Home
       </Button>
--- Flow Project/FE/src/utils/StylesHelper/ErrorPage.ts
+++ Flow Project/FE/src/utils/StylesHelper/ErrorPage.ts
@@ -1,4 +1,4 @@
-import { Theme } from "@mui/material";
+import { SxProps, Theme } from "@mui/material";
 import { CSSProperties } from "react";
 
 export const errorPageStyles = (theme: Theme): CSSProperties => ({
@@ -9,4 +9,16 @@ export const errorPageStyles = (theme: Theme): CSSProperties => ({
   color: theme.palette.primary.main,
   height: "100%",
   gap: "4.8rem",
+  padding: "9.6rem 3.2rem",
 });
+
+export const errorP = (theme: Theme): SxProps => ({
+  fontSize: "2rem",
+  maxWidth: "600px",
+  textAlign: "center",
+  color: theme.palette.primary.main,
+});
+
+export const errorImgSadFace: SxProps = {
+  width: { xs: "16rem", sm: "24rem" },
+};

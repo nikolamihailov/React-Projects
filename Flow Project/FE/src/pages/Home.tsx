0413d10ab1b3e652b03be5e22fe3d071c5526c71
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-10-29 22:25:46 +0200

    scroll to top on home bumped

--- Flow Project/FE/src/pages/Home.tsx
+++ Flow Project/FE/src/pages/Home.tsx
@@ -2,9 +2,14 @@ import WelcomeBack from "../components/Sections/WelcomeBack/WelcomeBack";
 import HowItWorks from "../components/Sections/HowItWorks/HowItWorks";
 import FAQ from "../components/Sections/FAQ/FAQ";
 import useTitle from "../hooks/useTitle";
+import { useEffect } from "react";
 
 function Home() {
   useTitle("Flow - SPA and Fitness");
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>

b85cdd687c80b483d47c5a3605d6b44406ded64f
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-12-21 20:16:56 +0200

    refactor: update API integration and add planet names service

--- solar-system-planets/src/services/api.js
+++ solar-system-planets/src/services/api.js
@@ -1,29 +1,37 @@
-const BASE_URL = "https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/";
+// old API key is not working anymore
+// const BASE_URL = "https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/";
+// const API_KEY = import.meta.env.VITE_API_KEY;
+// const API_HOST = import.meta.env.VITE_API_HOST;
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-const API_HOST = import.meta.env.VITE_API_HOST;
+import { planetNames } from "./planetNames";
+
+const BASE_URL = "https://planets-17f2.onrender.com/planets/getPlanet?name=";
 
 const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': API_KEY,
-        'X-RapidAPI-Host': API_HOST
-    }
+  method: "GET",
+  /*  headers: {
+         'X-RapidAPI-Key': API_KEY,
+         'X-RapidAPI-Host': API_HOST
+     } */
 };
 
 const getAllPlanets = async function () {
-    const res = await fetch(BASE_URL, options);
-    const data = await res.json();
-    return data;
+  const mappedRequests = planetNames.map((planetName) => fetch(`${BASE_URL}${planetName}`));
+
+  const responses = await Promise.all(mappedRequests);
+
+  const data = await Promise.all(responses.map((res) => res.json()));
+
+  return data;
 };
 
-const getPlanet = async function (planetId) {
-    const res = await fetch(`${BASE_URL}${planetId}`, options);
-    const data = await res.json();
-    return data;
+const getPlanet = async function (planetName) {
+  const res = await fetch(`${BASE_URL}${planetName}`);
+  const data = await res.json();
+  return data;
 };
 
 export const planetService = {
-    getAllPlanets,
-    getPlanet
+  getAllPlanets,
+  getPlanet,
 };
---
+++ solar-system-planets/src/services/planetNames.js
@@ -0,0 +1,1 @@
+export const planetNames = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];

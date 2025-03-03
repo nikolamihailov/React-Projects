b76c04deba4e53cdce7d0a9d103baba7574148c5
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-03-03 23:55:33 +0200

    add new partners to array

--- sipspot/src/features/shared/components/Partners/partnersArray.ts
+++ sipspot/src/features/shared/components/Partners/partnersArray.ts
@@ -2,10 +2,20 @@ import logo1 from "/partnerLogo1.png";
 import logo2 from "/partnerLogo2.png";
 import logo3 from "/partnerLogo3.png";
 import logo4 from "/partnerLogo4.png";
+import logo5 from "/partnerLogo5.png";
+import logo6 from "/partnerLogo6.png";
+import logo7 from "/partnerLogo7.png";
+import logo8 from "/partnerLogo8.png";
+import logo9 from "/partnerLogo9.png";
 
 export const partners = [
     { id: 1, src: logo1 },
     { id: 2, src: logo2 },
     { id: 3, src: logo3 },
-    { id: 4, src: logo4 }
+    { id: 4, src: logo4 },
+    { id: 5, src: logo5 },
+    { id: 6, src: logo6 },
+    { id: 7, src: logo7 },
+    { id: 8, src: logo8 },
+    { id: 9, src: logo9 }
 ];

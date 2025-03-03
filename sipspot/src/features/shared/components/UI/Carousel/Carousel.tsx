82a88103499aec4820b5360f6822207124466578
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-03-03 23:55:47 +0200

    finsih partners section

--- sipspot/src/features/shared/components/UI/Carousel/Carousel.tsx
+++ sipspot/src/features/shared/components/UI/Carousel/Carousel.tsx
@@ -3,22 +3,27 @@ import { Gap } from "../../../../../types/Gap";
 type CarouselItem = {
     id: number | string;
     src: string;
-}
+};
+
 type CarouselProps = {
     items: CarouselItem[];
-    gap: Gap;
-}
+    gap?: Gap;
+};
 
-function Carousel({ items, gap= Gap.MEDIUM }: CarouselProps) {
+function Carousel({ items, gap = Gap.MEDIUM }: CarouselProps) {
+     const duplicatedItems = [...items, ...items];
+
     return (
-        <div className={`carousel carousel__gap--${gap}`}>
-            {items.length && items.map(item => {
-                return <div className="carousel__item" key={item.id}>
-                    <img src={item.src} alt="carousel-item-img" className="carousel__img" />
-                </div>
-            })}
+        <div className="carousel">
+            <div className={`carousel__track carousel__gap--${gap}`}>
+                {duplicatedItems.map((item, index) => (
+                    <div className="carousel__item" key={`${item.id}-${index}`}>
+                        <img src={item.src} alt="carousel-item-img" className="carousel__img" />
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
 
 export default Carousel;

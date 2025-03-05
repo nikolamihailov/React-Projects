4348564a956f2634b2777520fe70f06c16c20f60
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-03-05 22:08:33 +0200

    bump new hook and types

---
+++ sipspot/src/types/cocktails/Cocktail.ts
@@ -0,0 +1,30 @@
+export type Cocktail = {
+    idDrink: string;
+    strDrink: string;
+    strCategory: string;
+    strAlcocholic: string;
+    strGlass: string;
+    strInstructions: string;
+    strDrinkThumb: string;
+    strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3: string;
+    strIngredient4: string;
+    strIngredient5: string;
+    strIngredient6: string;
+    strIngredient7: string;
+    strIngredient8: string;
+    strIngredient9: string;
+    strIngredient10: string;
+    strMeasure1: string;
+    strMeasure2: string;
+    strMeasure3: string;
+    strMeasure4: string;
+    strMeasure5: string;
+    strMeasure6: string;
+    strMeasure7: string;
+    strMeasure8: string;
+    strMeasure9: string;
+    strMeasure10: string;
+    strImageSource: string;
+}
---
+++ sipspot/src/types/cocktails/CocktailsResults.ts
@@ -0,0 +1,5 @@
+import { Cocktail } from "./Cocktail"
+
+export type CocktailsResults = {
+    drinks: Cocktail[];
+}

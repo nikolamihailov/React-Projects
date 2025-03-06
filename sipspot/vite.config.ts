154fa36c35cbe89e1432e41d1382ef854c9328a4
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-03-06 22:38:03 +0200

    update vite config for lattest scss

--- sipspot/vite.config.ts
+++ sipspot/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            less: {
+                math: "parens-division",
+            },
+            scss: {
+                api: "modern-compiler", // or "modern", "legacy"
+                importers: [
+                    // ...
+                ],
+            },
+        },
+    },
+});

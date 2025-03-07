b2f80e255686f27f80f54989cf22f568d4680cb7
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-03-07 23:18:27 +0200

    finish featured-cocktails section

--- sipspot/src/features/cocktails/FeaturedCocktails/FeaturedCocktailsSection.tsx
+++ sipspot/src/features/cocktails/FeaturedCocktails/FeaturedCocktailsSection.tsx
@@ -7,6 +7,14 @@ import mojitoPng from "/mojito.png";
 function FeaturedCocktailsSection() {
     return (
         <Section sectionClassName="featured-cocktails">
+            <div className="featured-cocktails__info">
+                <h2 className="heading-secondary">Our most popular cocktails:</h2>
+                <p className="paragraph">
+                    Discover our selection of best-loved cocktails, crafted to perfection. Whether you enjoy the
+                    refreshing zest of a Mojito, the sweet and tangy flavors of a Strawberry Margarita, or the smooth
+                    balance of a classic Daiquiri, there's a drink here to delight every palate.
+                </p>
+            </div>
             <div className="featured-cocktails__container">
                 <FeaturedItem name="Daiquiri" imgSrc={daiquiriPng} />
                 <FeaturedItem name="Mojito" imgSrc={mojitoPng} />

6d7752d2ffffa31861d30b5c71bcecd617e46a39
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-03-17 23:32:08 +0200

    start new section

---
+++ sipspot/src/features/shared/components/Features/FeaturesSection.tsx
@@ -0,0 +1,11 @@
+import Section from "../UI/Section/Section";
+
+function FeaturesSection() {
+    return (
+        <Section sectionClassName="features">
+            <h2>Features</h2>
+        </Section>
+    );
+}
+
+export default FeaturesSection;

5dda91905573d307341703826f37400c06fc4044
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-03-17 23:30:15 +0200

    rename component

--- sipspot/src/routes/routes.tsx
+++ sipspot/src/routes/routes.tsx
@@ -5,34 +5,34 @@ import Home from "../pages/Home";
 import Recipes from "../pages/Recipes";
 import Gallery from "../pages/Gallery";
 import About from "../pages/About";
-import ErrorPage from "../pages/ErrorPage";
+import ErrorPage from "../pages/Error";
 
 export const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        element: <Home />,
-        index: true,
-      },
-      {
-        path: "/recipes",
-        element: <Recipes />,
-      },
-      {
-        path: "/gallery",
-        element: <Gallery />,
-      },
-      {
-        path: "/about-us",
-        element: <About />,
-      },
-      {
-        path: "*",
-        element: <ErrorPage />,
-      },
-    ],
-  },
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                element: <Home />,
+                index: true,
+            },
+            {
+                path: "/recipes",
+                element: <Recipes />,
+            },
+            {
+                path: "/gallery",
+                element: <Gallery />,
+            },
+            {
+                path: "/about-us",
+                element: <About />,
+            },
+            {
+                path: "*",
+                element: <ErrorPage />,
+            },
+        ],
+    },
 ]);

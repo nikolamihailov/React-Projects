1be1d8f25b86048af03ba36df2dc38e20c903b99
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-03-17 23:31:23 +0200

    create footer component and add it to layout

--- sipspot/src/features/shared/components/Footer/Footer.tsx
+++ sipspot/src/features/shared/components/Footer/Footer.tsx
@@ -1,5 +1,48 @@
+import { NavLink } from "react-router-dom";
+import SocialNetwork from "../Socials/SocialNetwork";
+import { SocialNetworks, socials } from "../../../../types/Socials";
+
 function Footer() {
-    return <footer className="footer"></footer>;
+    return (
+        <footer className="footer">
+            <div className="footer__logo-terms">
+                <picture className="footer__logo">
+                    <source srcSet="/logo-icon.png 1x, /logo.png 2x" media="(max-width: 60rem)" />
+                    <img alt="Full logo" src=" /logo.png" />
+                </picture>
+                <p className="footer__terms">© {new Date().getFullYear()} All rights reserved.</p>
+            </div>
+            <div className="footer__links">
+                <ul className="footer__nav-list">
+                    <li className="footer__nav-item">
+                        <NavLink to={"/"}>Home</NavLink>
+                    </li>
+                    <li className="footer__nav-item">
+                        <NavLink to={"/recipes"}>Recipes</NavLink>
+                    </li>
+                    <li className="footer__nav-item">
+                        <NavLink to={"/gallery"}>Gallery</NavLink>
+                    </li>
+                    <li className="footer__nav-item">
+                        <NavLink to={"/about-us"}>About us</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__socials-design">
+                <ul className="footer__socials">
+                    <SocialNetwork linkUrl={socials[SocialNetworks.linkedIn]} networkName={SocialNetworks.linkedIn} />
+                    <SocialNetwork linkUrl={socials[SocialNetworks.github]} networkName={SocialNetworks.github} />
+                    <SocialNetwork linkUrl={socials[SocialNetworks.instagram]} networkName={SocialNetworks.instagram} />
+                </ul>
+                <p className="footer__creator">
+                    Design:
+                    <a href="https://github.com/nikolamihailov" target="_blank">
+                        Nikola Mihaylov
+                    </a>
+                </p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- sipspot/src/pages/Layout.tsx
+++ sipspot/src/pages/Layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../features/shared/components/Header/Header";
+import Footer from "../features/shared/components/Footer/Footer";
 
 function Layout() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default Layout;

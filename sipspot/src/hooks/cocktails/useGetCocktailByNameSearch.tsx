680fff51bf45ec6e82e6affb9da50be5e42e2bce
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-05-23 23:22:22 +0300

    extract key to const

--- sipspot/src/hooks/cocktails/useGetCocktailByNameSearch.tsx
+++ sipspot/src/hooks/cocktails/useGetCocktailByNameSearch.tsx
@@ -2,20 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { getCocktailByName } from "../../services/cocktails/cocktailService";
 
-export const useGetCocktailByNameSearch = (name: string) => {
+const QUERY_KEY_NAME = "get cocktail by search name";
 
-  return useQuery({
-    queryKey: ["get cocktail by search name", name],
-    queryFn: async () => {
-      try {
-        const response = await getCocktailByName(name);
-        return response;
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.error(error);
-        }
-      }
-    },
-    enabled: !!name,
-  });
+export const useGetCocktailByNameSearch = (name: string) => {
+    return useQuery({
+        queryKey: [QUERY_KEY_NAME, name],
+        queryFn: async () => {
+            try {
+                const response = await getCocktailByName(name);
+                return response;
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    console.error(error);
+                }
+            }
+        },
+        enabled: !!name,
+    });
 };

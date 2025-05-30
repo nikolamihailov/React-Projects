eab347939b42a675254fa1aff41567db9e753865
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-05-30 23:39:52 +0300

    start testing testimonial variants

--- sipspot/src/features/shared/components/Sections/Testimonials/TestimonialsSection.tsx
+++ sipspot/src/features/shared/components/Sections/Testimonials/TestimonialsSection.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import SectionInfo from "../../SectionInfo";
 import Section from "../../UI/Section/Section";
 
 function TestimonialsSection() {
+    const [cats] = useState(["Bad", "Stava", "Top", "Tragediq"]);
+
     return (
         <Section sectionClassName="testimonials">
             <SectionInfo heading="What Our Customers Say:" subheading="Testimonials" />
+            {cats.map((cat) => {
+                return <div>{cat}</div>;
+            })}
         </Section>
     );
 }
